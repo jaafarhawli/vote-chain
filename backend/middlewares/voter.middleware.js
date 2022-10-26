bd0fe4591107ebf0cb902801d6a3de97586b894d
JaafarHawli <jaafarhawle@gmail.com>
2022-10-26 17:13:17 +0300

    backend: added voter authorization middleware

---
+++ backend/middlewares/voter.middleware.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const Voter = require('../models/voters.model');
+
+const voterMiddleware = async (req, res, next) => {
+    if(!req.headers.authorization)
+    return res.status(401).json({message: "Unauthorized"})
+    const token = req.headers.authorization.split(" ")[1];
+
+    if(!token) return res.status(401).json({message: "Unauthorized"})
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        const voter = await Voter.findOne({email: decoded.email}).lean();
+        req.voter = {...voter};
+        next()
+
+    }catch(err){
+        return res.status(401).json({message: "Unauthorized"})
+    }
+
+}
+
+module.exports = voterMiddleware;
