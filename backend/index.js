364c63289e47055caeafe7014c51477a7c953793
JaafarHawli <jaafarhawle@gmail.com>
2022-10-26 18:45:47 +0300

    backend: added auth.routes to index.js

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,9 @@ app.use(express.json());
 require('dotenv').config();
 require('./config/db.config');
 
+const authRoutes = require('./routes/auth.routes');
+app.use('/auth', authRoutes);
+
 app.listen(process.env.PORT, (err)=>{
     if(err) throw err;
     console.log(`server running on port ${process.env.PORT}`);
