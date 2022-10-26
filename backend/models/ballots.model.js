7efae2877bd37d9c569f4f6d24b75b0eb30bdc8c
JaafarHawli <jaafarhawle@gmail.com>
2022-10-26 16:09:52 +0300

    backend: added ballots model

---
+++ backend/models/ballots.model.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const ballotSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: 'Title is required',
+    }, 
+    
+    start_time: {
+        type: Date,
+        required: 'Start time is required',
+    }, 
+    
+    end_time: {
+        type: Date,
+        required: 'End time is required',
+    },
+    
+    timezone: {
+        type: String,
+        required: 'Time zone is required',
+    },
+
+    admin: {
+        type: Schema.Types.ObjectId, ref: 'User'
+    },
+    
+    moderators: [{
+        type: Schema.Types.ObjectId, ref: 'User'
+    }],
+    
+    voters: [{
+        type: Schema.Types.ObjectId, ref: 'Voter'
+    }],
+
+    parties: [{
+        type: Schema.Types.ObjectId, ref: 'Party'
+    }],
+})
+
+const Ballot = mongoose.model('Ballot', ballotSchema);
+
+module.exports = Ballot;
