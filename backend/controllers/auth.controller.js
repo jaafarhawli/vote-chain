9239bcd2fadb6a68b72171aca28976373900d92f
JaafarHawli <jaafarhawle@gmail.com>
2022-10-26 16:53:26 +0300

    backend: added userSignup function

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const User = require('../models/users.model');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+var validator = require("email-validator");
 
 const login = async (req, res)=>{
     const {email, password} = req.body;
@@ -18,7 +19,39 @@ const login = async (req, res)=>{
     res.status(200).json(token);
 }
 
+const userSignup = async (req, res)=>{
+    const {email, password, first_name, last_name} = req.body;
+
+    if(password.length < 8)
+    return res.status(400).json("Invalid input");
+     
+    const validate = validator.validate(email); 
+    if(!validate)
+    return res.status(400).json("Invalid input");
+    
+    try{
+        const user = new User();
+        user.email = email;
+        user.password = await bcrypt.hash(password, 10);
+        user.first_name = first_name;
+        user.last_name = last_name;
+        await user.save();
+
+        const token = jwt.sign({email: user.email}, process.env.JWT_SECRET_KEY, {
+            expiresIn: '1y'
+        });
+
+        res.status(200).json({user, token});
+
+    }catch(err){
+        res.status(400).json({
+            message: err.message
+        })
+    }
+}
+
 
 module.exports = {
-    login
+    login,
+    userSignup
 }
