815577f818b128c806d7f38ac8bcc088b227d868
JaafarHawli <jaafarhawle@gmail.com>
2022-10-29 02:36:55 +0300

    frontend: installed and configured tailwind

--- frontend/src/pages/Landing Page/Landing.jsx
+++ frontend/src/pages/Landing Page/Landing.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 const Landing = () => {
   return (
     <div>
-      
+      <section className=''>
+          <nav className=''>
+            
+          </nav>
+      </section>
     </div>
   );
 }
