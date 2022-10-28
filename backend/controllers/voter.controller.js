e361a6ad04c1543a81c7ae26f15babaff85252c7
JaafarHawli <jaafarhawle@gmail.com>
2022-10-28 03:43:17 +0300

    backend: added getVoter function

--- backend/controllers/voter.controller.js
+++ backend/controllers/voter.controller.js
@@ -3,3 +3,16 @@ const Election = require('../models/elections.model');
 const Voter = require('../models/voters.model');
 const Party = require('../models/parties.model');
 
+const getVoter = async (req, res) => {
+    const {voter_id} = req.params;
+    Voter.findOne({voter_id:voter_id}, async (err, voter) => {
+        if(err)
+        return res.status(404).json("Voter not found");
+        return res.status(200).json(voter);
+    })
+}
+
+
+module.exports = {
+    getVoter,
+}
