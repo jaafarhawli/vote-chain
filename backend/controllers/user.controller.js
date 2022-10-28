446fe66188db65462ea3193b89e6b1d2f05729e3
JaafarHawli <jaafarhawle@gmail.com>
2022-10-28 03:04:03 +0300

    backend: modified addModerator function

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -147,6 +147,8 @@ const addModerator = async (req, res) => {
         return res.status(404).json("User not found");
         if(!user)
         return res.status(404).json("User not found");
+        if(user.elections.includes(election_id))
+        return res.status(400).json("Invalid request");
         Election.findById(election_id, async (err, election) => {
             if(err) 
             return res.status(404).json("Election not found");
