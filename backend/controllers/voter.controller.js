bd21a9f921c0e9c66c56d39da2bf4c8cb162adc8
JaafarHawli <jaafarhawle@gmail.com>
2022-10-28 04:45:00 +0300

    backend: added vote function

--- backend/controllers/voter.controller.js
+++ backend/controllers/voter.controller.js
@@ -1,4 +1,3 @@
-const User = require('../models/users.model');
 const Election = require('../models/elections.model');
 const Voter = require('../models/voters.model');
 const Party = require('../models/parties.model');
@@ -14,11 +13,6 @@ const getVoter = async (req, res) => {
 
 const viewElectionAsVoter = async (req, res) => {
     const {email, election_id} = req.params;
-    // Election.findById(election_id, (err, election) => {
-    //     if(err) 
-    //     return res.status(404).json("Election not found");
-        
-    // });
     const election = await Election.findById(election_id).select("-admin -moderators");
     if(!election)
     return res.status(404).json("Election not found");
@@ -27,8 +21,32 @@ const viewElectionAsVoter = async (req, res) => {
     res.status(200).json(election);
 }
 
+const vote = async (req, res) => {
+    const {id, party_id,candidate_id} = req.body;
+    const voter = await Voter.findById(id).select();
+    if(!voter)
+    return res.status(404).json("Voter not found");
+    if(voter.voted == 1)
+    return res.status(400).json("Already voted");
+    
+    const party = await Party.findOne({_id: party_id}).select();
+    if(!party)
+    return res.status(404).json("Party not found");
+    party.candidates.forEach((candidate) => {
+        if(candidate._id == candidate_id) {
+            candidate.score+=1;
+        }
+    })
+    party.save();
+    voter.voted = 1;
+    voter.voting_time = new Date(Date.now()).toUTCString();
+    await voter.save();
+    return res.status(200).json("Voted Successfully");
+}
+
 
 module.exports = {
     getVoter,
-    viewElectionAsVoter
+    viewElectionAsVoter,
+    vote
 }
--- backend/routes/voter.routes.js
+++ backend/routes/voter.routes.js
@@ -1,9 +1,10 @@
 const {Router} = require('express');
-const {getVoter, viewElectionAsVoter} = require('../controllers/voter.controller');
+const {getVoter, viewElectionAsVoter, vote} = require('../controllers/voter.controller');
 const voterMiddleware = require('../middlewares/voter.middleware');
 const router = Router();
 
 router.get('/:voter_id', voterMiddleware, getVoter);
 router.get('/election/:email/:election_id', voterMiddleware, viewElectionAsVoter);
+router.post('/vote', voterMiddleware, vote);
 
 module.exports = router;
