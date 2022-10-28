2d0f7e5175f32f5bf342ad839f2f686d63dd8f66
JaafarHawli <jaafarhawle@gmail.com>
2022-10-28 03:42:52 +0300

    backend: added voter controller

---
+++ backend/controllers/voter.controller.js
@@ -0,0 +1,5 @@
+const User = require('../models/users.model');
+const Election = require('../models/elections.model');
+const Voter = require('../models/voters.model');
+const Party = require('../models/parties.model');
+
