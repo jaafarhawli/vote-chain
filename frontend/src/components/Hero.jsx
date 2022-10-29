e167f9f57bfb7019bd813cbffe41cc53316bfd64
JaafarHawli <jaafarhawle@gmail.com>
2022-10-30 00:33:05 +0200

    frontend: added curve to hero component

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from '../assets/VOTE CHAIN-logo-white-horizantal.png';
 import landingsm from '../assets/landing-sm.svg';
 import landinglg from '../assets/landing-lg.svg';
+import curve from '../assets/curve.svg';
 import {FaRegPlusSquare} from 'react-icons/fa';
 
 const Hero = () => {
   return (
+    <div>
     <div className='flex flex-col bg-gradient-to-b from-black-100 to to-black-200 min-h-[500px]'>
       <div className='flex w-full pt-4 items-center justify-between lg:px-28 md:px-10 px-4'>
         <img src={logo} alt="logo" className='w-48' />
@@ -20,9 +22,11 @@ const Hero = () => {
             <p className='text-white pt-6'>Create and launch your election securely<br/>from your own device</p>
             <button className='mt-6 max-w-[300px] gap-2'>Create a new election <FaRegPlusSquare className='text-[18px]' /> </button>
         </div>
-        <img src={landingsm} alt="talents" className="w-[50%] md:hidden hidden sm:flex" />
-        <img src={landinglg} alt="talents" className="w-[40%] hidden md:flex" />
+        <img src={landingsm} alt="talents" className="w-[50%] md:hidden hidden sm:flex mt-5" />
+        <img src={landinglg} alt="talents" className="w-[45%] hidden md:flex mt-5" />
       </div>
+    </div>
+      <div className='aspect-[960/200] w-full bg-no-repeat bg-center bg-cover'  style={{backgroundImage: `url(${curve})`}}></div>
     </div>
   );
 }
