f0000e9711f7be9e6bfc5a48982b1b278c153aa4
JaafarHawli <jaafarhawle@gmail.com>
2022-10-29 04:37:39 +0300

    frontend: added hero content

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import logo from '../assets/VOTE CHAIN-logo-white-horizantal.png';
+import {FaRegPlusSquare} from 'react-icons/fa';
 
 const Hero = () => {
   return (
-    <div className='bg-gradient-to-b from-black-100 to to-black-200 h-96 px-16'>
+    <div className='flex flex-col bg-gradient-to-b from-black-100 to to-black-200 min-h-[500px] lg:px-16 md:px-10 px-4'>
       <div className='flex w-full py-4 items-center justify-between'>
         <img src={logo} alt="logo" className='w-48' />
         <div className='flex gap-4'>
             <button className='border-cyan border-2 bg-opacity-0 hover:border-white'>Login</button>
             <button>Register</button>
+        </div>
+      </div>
+      <div className='flex h-full flex-1'>
+        <div className='flex flex-col  justify-center'>
+            <h1 className='text-4xl text-white '><span className='text-purple font-semibold'>Blockchain-based</span><br/>online voting system </h1>
+            <p className='text-white pt-6'>Create and launch your election securely<br/>from your own device</p>
+            <button className='mt-6 max-w-[300px] gap-2'>Create a new election <FaRegPlusSquare className='text-[18px]' /> </button>
         </div>
       </div>
     </div>
