3c86c953fbc3fa686e710cb74c7fe60f64bc4d48
JaafarHawli <jaafarhawle@gmail.com>
2022-10-30 11:24:19 +0200

    frontend: added useStates for form inputs

--- frontend/src/components/RegisterForm.jsx
+++ frontend/src/components/RegisterForm.jsx
@@ -4,6 +4,16 @@ import logo from '../assets/VOTE CHAIN-logo-black.png';
 import axios from '../api/axios';
 
 const RegisterForm = () => {
+
+  const navigate = useNavigate();
+
+  const [firstname, setFirstname] = useState();
+  const [lastname, setLastname] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [confirm, setConfirm] = useState();
+  const [error, setError] = useState(false);
+
   return (
     <div>
       
