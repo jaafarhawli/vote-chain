fbc5fcaf146218a8527d0c7c6e6f892735d40db9
JaafarHawli <jaafarhawle@gmail.com>
2022-10-30 08:15:08 +0200

    frontend: added router to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import Landing from './pages/Landing Page/Landing';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Landing />
-    </div>
+
+    	<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<Navigate to={<Landing />} />} />
+			
+				</Routes>
+    	</BrowserRouter>
+
   );
 }
 
