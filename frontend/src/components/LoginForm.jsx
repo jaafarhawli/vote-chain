d1c3aeeb2d7e6f66aa2223077bedbea9bd4a82b9
JaafarHawli <jaafarhawle@gmail.com>
2022-10-30 10:24:25 +0200

    frontend: added before pseudo element to login form

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/VOTE CHAIN-logo-black.png';
 
 const LoginForm = () => {
   return (
-    <div className='bg-gradient-to-r from-white/40 to-white/20 w-[400px]  rounded-xl flex flex-col items-center p-6 pb-10 backdrop-blur-xl shadow-2xl'>
+    <div className='bg-gradient-to-br from-white/70 to-white/30 w-[400px]  rounded-xl flex flex-col items-center p-6 pb-10 backdrop-blur-2xl shadow-2xl before:absolute before:bg-white/[15%] before:inset-0 before:rotate-[-5deg] before:-z-[1] before:rounded-xl'>
       <img src={logo} alt="logo" className='w-[180px]' />
       <div className='bg-black-100 h-[2px] w-[180px]'></div>  
       <h1 className='mt-4 text-2xl font-semibold text-purple-100'>Welcome!</h1>  
@@ -18,7 +18,7 @@ const LoginForm = () => {
           </label>
           <button>Login</button>
       </form> 
-      <p className='mt-4 text-[16px]'>New to Vote Chain? <span className='font-semibold text-purple-100 underline'>Sign up</span></p>
+      <p className='mt-4 text-[16px]'>New to Vote Chain? <span className='font-semibold text-purple-100 select-none hover:underline'>Sign up</span></p>
     </div>
   );
 }
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import LoginForm from '../components/LoginForm';
+import banner from '../assets/banner.png';
 
 const Login = () => {
   return (
-    <div className='bg-gradient-to-r from-purple-100 to-purple-200  flex w-full h-screen justify-center items-center'>
-      <LoginForm />
+    <div className='bg-gradient-to-r from-purple-100 to-purple-200  flex w-full h-screen justify-center items-center md:justify-between'>
+      <div className='flex md:w-1/2 justify-center items-center'>
+          <LoginForm className='' />
+      </div>
+      <img src={banner} alt="/" className='hidden md:flex w-1/2 object-cover h-full' />
     </div>
   );
 }
