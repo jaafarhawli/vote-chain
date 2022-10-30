49ddd5a2054c0cf0e1e8a6ee09166a669554bcd5
JaafarHawli <jaafarhawle@gmail.com>
2022-10-30 09:02:58 +0200

    frontend: added login form component

---
+++ frontend/src/components/LoginForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoginForm = () => {
+  return (
+    <div className='bg-gradient-to-r from-white/40 to-white/20 w-[400px] h-2/3 rounded-xl flex flex-col items-center p-12 backdrop-blur-xl shadow-2xl'>
+      
+    </div>
+  );
+}
+
+export default LoginForm;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import LoginForm from '../components/LoginForm';
 
 const Login = () => {
   return (
-    <div className='bg-purple-200 flex w-full h-screen'>
-      
+    <div className='bg-gradient-to-r from-purple-100 to-purple-200  flex w-full h-screen justify-center items-center'>
+      <LoginForm />
     </div>
   );
 }
