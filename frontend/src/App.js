9fd6a65da0e4fa86a2ba6640ac2bb042fe2ed86c
JaafarHawli <jaafarhawle@gmail.com>
2022-10-30 08:19:20 +0200

    frontend: installed react query and added it to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,23 @@
 import './App.css';
 import Landing from './pages/Landing Page/Landing';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
-  return (
 
+  const client = new QueryClient();
+
+  return (
+    <QueryClientProvider client={client}>
     	<BrowserRouter>
 				<Routes>
 					<Route path='/' element={<Navigate to={<Landing />} />} />
-			
+					{/* <Route path='/login' element={<Login />} />
+					<Route path='/register' element={<Register />} />
+					<Route path='/main/*' element={<MainPage />} /> */}
 				</Routes>
     	</BrowserRouter>
-
+	</QueryClientProvider>
   );
 }
 
