bc3db0a83ce336ad52b78945499b5c6283eb0b69
JaafarHawli <jaafarhawle@gmail.com>
2022-10-30 10:35:07 +0200

    frontend: added useState to input values

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
 import logo from '../assets/VOTE CHAIN-logo-black.png';
 
 const LoginForm = () => {
+
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
   return (
     <div className='bg-gradient-to-br from-white/70 to-white/30 w-[400px]  rounded-xl flex flex-col items-center p-6 pb-10 backdrop-blur-2xl shadow-2xl before:absolute before:bg-white/[15%] before:inset-0 before:rotate-[-5deg] before:-z-[1] before:rounded-xl'>
       <img src={logo} alt="logo" className='w-[180px]' />
@@ -10,15 +17,15 @@ const LoginForm = () => {
       <form className='w-4/5 flex flex-col gap-5 mt-4'>
           <label>
               <p className='font-medium'>Email</p>
-              <input type="email"/>
+              <input type="email" onChange={e => setEmail(e.target.value)}/>
           </label>
           <label>
               <p className='font-medium'>Password</p>
-              <input type="password"/>
+              <input type="password" onChange={e => setPassword(e.target.value)}/>
           </label>
-          <button>Login</button>
+          <button onClick={handleSubmit}>Login</button>
       </form> 
-      <p className='mt-4 text-[16px]'>New to Vote Chain? <span className='font-semibold text-purple-100 select-none hover:underline'>Sign up</span></p>
+      <p className='mt-4 text-[16px]'>New to Vote Chain? <span className='font-semibold text-purple-100 select-none hover:underline' onClick={() => navigate('/register')}>Sign up</span></p>
     </div>
   );
 }
