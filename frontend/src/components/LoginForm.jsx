0289682ebe52498b8f5c9442f2d1cee07f15f368
JaafarHawli <jaafarhawle@gmail.com>
2022-10-30 10:46:22 +0200

    linking: linked login form to login api

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import logo from '../assets/VOTE CHAIN-logo-black.png';
+import axios from '../../api/axios';
+import jwt_decode from "jwt-decode";
 
 const LoginForm = () => {
 
@@ -8,12 +10,44 @@ const LoginForm = () => {
 
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+  const [error, setError] = useState(false);
+
+  const handleSubmit = async () => {
+    const form = {
+        email: email,
+        password: password
+    };
+    try {
+        const data = await axios.post('auth/login/user', form);
+        const token = data.data;
+        const decoded = jwt_decode(token);
+        localStorage.setItem('token', token);
+        localStorage.setItem('email', decoded.email);
+        try {
+          const user = await axios.get(`user/${decoded.email}`, {
+            headers: {
+              Authorization: `bearer ${localStorage.token}`
+            }
+          });
+          localStorage.setItem('firstname', data.first_name);
+          localStorage.setItem('lastname', data.last_name)
+          navigate('/main');
+        } catch (error) {
+          console.log(error);
+        }
+    }
+    catch (error) {
+        setError(true);
+        console.log(error);
+    }
+  }
 
   return (
     <div className='bg-gradient-to-br from-white/70 to-white/30 w-[400px]  rounded-xl flex flex-col items-center p-6 pb-10 backdrop-blur-2xl shadow-2xl before:absolute before:bg-white/[15%] before:inset-0 before:rotate-[-5deg] before:-z-[1] before:rounded-xl'>
       <img src={logo} alt="logo" className='w-[180px]' />
       <div className='bg-black-100 h-[2px] w-[180px]'></div>  
       <h1 className='mt-4 text-2xl font-semibold text-purple-100'>Welcome!</h1>  
+      <h1 className={error? 'error-message' : 'error-message display'}>Invalid Credentials</h1>
       <form className='w-4/5 flex flex-col gap-5 mt-4'>
           <label>
               <p className='font-medium'>Email</p>
