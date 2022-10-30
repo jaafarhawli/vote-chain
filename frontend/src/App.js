2a07843a72325bf8bacae0b2d2e03f0d03850044
JaafarHawli <jaafarhawle@gmail.com>
2022-10-30 08:31:00 +0200

    frontend: added functionality to hero buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ function App() {
     <QueryClientProvider client={client}>
     	<BrowserRouter>
 				<Routes>
-					<Route path='/' element={<Navigate to={<Landing />} />} />
+					<Route path='/' element={<Landing />} />
 					{/* <Route path='/login' element={<Login />} />
 					<Route path='/register' element={<Register />} />
 					<Route path='/main/*' element={<MainPage />} /> */}
--- frontend/src/components/LandingHero.jsx
+++ frontend/src/components/LandingHero.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import logo from '../assets/VOTE CHAIN-logo-white-horizantal.png';
 import landingsm from '../assets/landing-sm.svg';
 import landinglg from '../assets/landing-lg.svg';
@@ -6,20 +7,23 @@ import curve from '../assets/backgroundcurve.svg';
 import {FaRegPlusSquare} from 'react-icons/fa';
 
 const LandingHero = () => {
+
+  const navigate = useNavigate();
+
   return (
     <div className='hero flex flex-col bg-no-repeat bg-center bg-cover bg-purple-300 w-full' >
       <div className='flex w-full pt-4 items-center justify-between lg:px-28 md:px-10 px-4'>
         <img src={logo} alt="logo" className='w-48' />
         <div className='flex gap-4'>
-            <button className='border-cyan border-2 bg-opacity-0 hover:border-white'>Login</button>
-            <button>Register</button>
+            <button className='border-cyan border-2 bg-opacity-0 hover:border-white' onClick={() => navigate('/login')}>Login</button>
+            <button onClick={() => navigate('/register')}>Register</button>
         </div>
       </div>
       <div className='flex h-full flex-1 lg:pl-28 md:pl-10 pl-4 sm:justify-between justify-center'>
         <div className='flex flex-col justify-center sm:items-start items-center text-center sm:text-left'>
             <h1 className='text-4xl text-white md:text-5xl lg:text-6xl pt-12 md:pt-8'><span className='text-purple-100 font-semibold'>Blockchain-based</span><br/>online voting system </h1>
             <p className='text-white pt-6'>Create and launch your election securely<br/>from your own device</p>
-            <button className='mt-6 max-w-[300px] gap-2'>Create a new election <FaRegPlusSquare className='text-[18px]' /> </button>
+            <button className='mt-6 max-w-[300px] gap-2' onClick={() => navigate('/login')}>Create a new election <FaRegPlusSquare className='text-[18px]' /> </button>
         </div>
         <img src={landingsm} alt="talents" className="w-[45%] md:hidden hidden sm:flex mt-5" />
         <img src={landinglg} alt="talents" className="w-[45%] hidden md:flex mt-5" />
