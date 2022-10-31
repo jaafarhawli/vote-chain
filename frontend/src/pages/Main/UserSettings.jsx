075ea928240c332d43b3ad69d9073fd3c4ad4342
JaafarHawli <jaafarhawle@gmail.com>
2022-10-31 11:25:18 +0200

    frontend: added change password form

--- frontend/src/pages/Main/UserSettings.jsx
+++ frontend/src/pages/Main/UserSettings.jsx
@@ -26,7 +26,18 @@ const UserSettings = () => {
           </label>
           <button>Save changes</button>
         </form>
-      
+        <form className='mt-12 w-2/5 flex flex-col gap-5 lg:px-28 md:px-10 px-4'>
+          <h1 className='text-[28px] font-semibold text-purple-100'>Change Password</h1>
+            <label>
+                <p className='font-medium'>Old Password</p>
+                <input type="password" className=' border-[1px] border-black-200' onChange={e => setFirstname(e.target.value)}/>
+            </label>
+            <label>
+                <p className='font-medium'>New Password</p>
+                <input type="text" className=' border-[1px] border-black-200' onChange={e => setLastname(e.target.value)}/>
+            </label>
+          <button>Save changes</button>
+        </form>
     </div>
   );
 }
