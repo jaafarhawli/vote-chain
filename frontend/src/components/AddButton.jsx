268b13c0315c7efe90fbd73eeff9913e2d52403c
JaafarHawli <jaafarhawle@gmail.com>
2022-10-31 08:42:23 +0200

    frontend: modified MainHeader component

--- frontend/src/components/AddButton.jsx
+++ frontend/src/components/AddButton.jsx
@@ -4,7 +4,11 @@ import {FaRegPlusSquare} from 'react-icons/fa';
 const AddButton = (props) => {
   return (
     <div>
-      <button>{props.text} <FaRegPlusSquare className='text-[18px]' /></button>
+      <button>
+          <div className='flex items-center gap-1 justify-center'>
+              {props.text} <FaRegPlusSquare className='text-[18px]' />          
+          </div>
+          </button>
     </div>
   );
 }
--- frontend/src/components/MainHeader.jsx
+++ frontend/src/components/MainHeader.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from '../assets/VOTE CHAIN-logo-white-horizantal.png';
 import curve from '../assets/main-curve.svg';
 import { useNavigate } from 'react-router-dom';
+import AddButton from './AddButton';
 
 const MainHeader = (props) => {
 
@@ -20,6 +21,7 @@ const MainHeader = (props) => {
             </div>
         </div>
         <h1 className='text-white text-[28px] md:text-[32px] lg:text-[36px] font-bold mt-8'>Your Elections</h1>
+            
       </div>
       <div className='aspect-960/200 w-full  bg-no-repeat bg-cover bg-center md:[100px] h-[60px]' style={{backgroundImage: `url("${curve}")`}}></div>
     </div>
@@ -36,7 +38,10 @@ const MainHeader = (props) => {
                 <button className=''>{localStorage.firstname} {localStorage.lastname}</button>
             </div>
         </div>
-        <h1 className='text-white text-[28px] md:text-[32px] lg:text-[36px] font-bold mt-8'>Your Elections</h1>
+        <div className='flex justify-between w-full items-center mt-8'>
+            <h1 className='text-white text-[28px] md:text-[32px] lg:text-[36px] font-bold '>Your Elections</h1>
+            <AddButton text={"Create a new election"} />
+        </div>
       </div>
       <div className='aspect-960/200 w-full  bg-no-repeat bg-cover bg-center md:[100px] h-[60px]' style={{backgroundImage: `url("${curve}")`}}></div>
     </div>
