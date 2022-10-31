162d18868bf2d38ac2217a6de7ebcda3dfb9632d
JaafarHawli <jaafarhawle@gmail.com>
2022-10-31 06:49:19 +0200

    linking: linked viewElectionsAsAdmin to frontend

--- frontend/src/pages/Main/UserElections.jsx
+++ frontend/src/pages/Main/UserElections.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import {useQuery} from '@tanstack/react-query';
+import axios from '../../api/axios';
+import { useNavigate } from 'react-router-dom';
+import MainHeader from '../../components/MainHeader';
 
 const UserElections = () => {
+
+  const navigate = useNavigate();
+
+  const {data} = useQuery(["admin"], async () => {
+    return axios.get(`user/elections/${localStorage.id}`, {
+                headers: {
+                  Authorization: `bearer ${localStorage.token}`
+                }
+              }).then((res) => res.data);
+})
+
+
+
   return (
     <div>
+      <MainHeader />
       
     </div>
   );
 }
 
 export default UserElections;
+
+
+
