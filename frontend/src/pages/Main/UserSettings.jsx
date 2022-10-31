04639a85647d0f1d88d686cc2d7c85801349dbf5
JaafarHawli <jaafarhawle@gmail.com>
2022-10-31 11:37:04 +0200

    frontend: added responsive styling to settings page

--- frontend/src/pages/Main/UserSettings.jsx
+++ frontend/src/pages/Main/UserSettings.jsx
@@ -10,7 +10,7 @@ const UserSettings = () => {
   return (
     <div>
       <MainHeader title={'Account Settings'} empty={true} />
-      <form className='w-2/5 flex flex-col gap-5 lg:px-28 md:px-10 px-4'>
+      <form className='lg:w-[600px] w-[400px] flex flex-col gap-5 lg:px-28 md:px-10 px-4'>
           <h1 className='text-[28px] font-semibold text-purple-100'>Account Info</h1>
             <label>
                 <p className='font-medium'>First Name</p>
@@ -26,7 +26,7 @@ const UserSettings = () => {
           </label>
           <button>Save changes</button>
         </form>
-        <form className='mt-12 w-2/5 flex flex-col gap-5 lg:px-28 md:px-10 px-4'>
+        <form className='my-12 lg:w-[600px] w-[400px] flex flex-col gap-5 lg:px-28 md:px-10 px-4'>
           <h1 className='text-[28px] font-semibold text-purple-100'>Change Password</h1>
             <label>
                 <p className='font-medium'>Old Password</p>
@@ -37,6 +37,7 @@ const UserSettings = () => {
                 <input type="text" className=' border-[1px] border-black-200' onChange={e => setLastname(e.target.value)}/>
             </label>
           <button>Save changes</button>
+          <button className='bg-red'>Delete Account</button>
         </form>
     </div>
   );
