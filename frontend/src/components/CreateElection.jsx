8e4ac65707f44fb7aa9636f51db0e0d686c6d5c6
JaafarHawli <jaafarhawle@gmail.com>
2022-10-31 18:24:08 +0200

    frontend: added election modal content

--- frontend/src/components/CreateElection.jsx
+++ frontend/src/components/CreateElection.jsx
@@ -1,9 +1,62 @@
 import React from 'react';
+import {HiOutlineXMark} from 'react-icons/hi2';
+import axios from '../api/axios';
+import logo from '../assets/VOTE CHAIN-logo-white-horizantal.png';
 
-const CreateElection = () => {
-  return (
-    <div>
-      
+const CreateElection = ({open, closeModal}) => {
+
+    // const deleteAccount = async () => {
+    //     const form = {
+    //         id: localStorage.id
+    //     }
+        
+    //     try {
+    //         await axios.post('user/account', form, {
+    //             headers: {
+    //               Authorization: `bearer ${localStorage.token}`
+    //             }
+    //           });
+    //           localStorage.clear(); 
+    //           document.body.style.overflow = 'unset';
+    //           navigate('/');
+    //         } catch (error) {
+    //           console.log(error);
+    //         }
+    //     }
+    
+
+    if(!open)
+    return null;
+
+return (
+    <div className='bg-black-100/50 fixed w-full h-full z-10 '>
+     <div className=' fixed top-[50%] left-1/2 -translate-x-1/2 -translate-y-1/2 w-2/3 max-w-[400px] flex flex-col bg-white shadow-xl items-center z-10 rounded-lg px-8 py-14 '>
+         <HiOutlineXMark className='fixed top-2 left-2 text-[30px] hover:bg-black-100/20 rounded-full duration-200 p-1' onClick={closeModal} />
+         <img src={logo} alt="logo" className='w-[180px]' />
+      <div className='bg-black-100 h-[2px] w-[180px]'></div>  
+      <h1 className='my-4 text-2xl font-semibold text-purple-100'>New Election</h1>  
+      <form className='w-4/5 flex flex-col gap-5 '>
+          <div className='flex gap-2'>
+            <label>
+                <p className='font-medium'>Start date</p>
+                <input type="text" />
+            </label>
+            <label>
+                <p className='font-medium'>End date</p>
+                <input type="text"/>
+            </label>
+          </div>
+          <label>
+              <p className='font-medium'>Election title</p>
+              <input type="text" />
+          </label>
+          <label>
+              <p className='font-medium'>Timezone</p>
+              <input type="text"/>
+          </label>
+          <button className='bg-cyan' type="button">Create election</button>
+      </form> 
+     </div>
     </div>
   );
 }
--- frontend/src/components/MainHeader.jsx
+++ frontend/src/components/MainHeader.jsx
@@ -4,11 +4,11 @@ import curve from '../assets/main-curve.svg';
 import { useNavigate } from 'react-router-dom';
 import AddButton from './AddButton';
 
-const MainHeader = (props) => {
+const MainHeader = ({title, empty, open}) => {
 
     const navigate = useNavigate();
 
-  if(props.empty)
+  if(empty)
   return (
     <div>
       <div className='flex flex-col lg:px-28 md:px-10 px-4 bg-gradient-to-b from-black-100 to-black-200 w-full pt-6'>
@@ -20,7 +20,7 @@ const MainHeader = (props) => {
                 <button onClick={() => navigate('/main/settings')}>{localStorage.firstname} {localStorage.lastname}</button>
             </div>
         </div>
-        <h1 className='text-white text-[28px] md:text-[32px] lg:text-[36px] font-bold mt-8'>{props.title}</h1>
+        <h1 className='text-white text-[28px] md:text-[32px] lg:text-[36px] font-bold mt-8'>{title}</h1>
             
       </div>
       <div className='aspect-960/200 w-full  bg-no-repeat bg-cover bg-center md:[100px] h-[60px]' style={{backgroundImage: `url("${curve}")`}}></div>
@@ -39,8 +39,8 @@ const MainHeader = (props) => {
             </div>
         </div>
         <div className='flex justify-between w-full items-center mt-8'>
-            <h1 className='text-white text-[28px] md:text-[32px] lg:text-[36px] font-bold '>{props.title}</h1>
-            <AddButton text={"Create a new election"} />
+            <h1 className='text-white text-[28px] md:text-[32px] lg:text-[36px] font-bold '>{title}</h1>
+            <AddButton text={"Create a new election"} click={open} />
         </div>
       </div>
       <div className='aspect-960/200 w-full  bg-no-repeat bg-cover bg-center md:[100px] h-[60px]' style={{backgroundImage: `url("${curve}")`}}></div>
