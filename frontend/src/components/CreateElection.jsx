213c688a3ba3d62ec01b30195b96b2dc6f409baa
JaafarHawli <jaafarhawle@gmail.com>
2022-10-31 20:22:51 +0200

    frontend: added error modal to create election

--- frontend/src/components/CreateElection.jsx
+++ frontend/src/components/CreateElection.jsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import {HiOutlineXMark} from 'react-icons/hi2';
 import axios from '../api/axios';
 import logo from '../assets/VOTE CHAIN-logo-black.png';
+import ErrorModal from './ErrorModal';
 
 const CreateElection = ({open, closeModal}) => {
 
@@ -9,6 +10,8 @@ const CreateElection = ({open, closeModal}) => {
     const [starttime, setStarttime] = useState();
     const [endtime, setEndtime] = useState();
     const [timezone, setTimezone] = useState();
+    const [errorModal, setErrorModal] = useState(false);
+    const [error, setError] = useState('');
 
     const createElection = async () => {
         const form = {
@@ -20,13 +23,15 @@ const CreateElection = ({open, closeModal}) => {
         }
         
         try {
-             const data = await axios.post('user/election', form, {
+             await axios.post('user/election', form, {
                 headers: {
                   Authorization: `bearer ${localStorage.token}`
                 }
               });
-              console.log(data);
+              {closeModal()};
             } catch (error) {
+                setError(error.message);
+                setErrorModal(true);
               console.log(error);
             }
         }
@@ -37,6 +42,7 @@ const CreateElection = ({open, closeModal}) => {
 
 return (
     <div className='bg-black-100/50 fixed w-full h-full z-10 '>
+        <ErrorModal open={errorModal} error={error} closeError={() => setErrorModal(false)} />
      <div className=' fixed top-[50%] left-1/2 -translate-x-1/2 -translate-y-1/2 md:w-2/3 w-4/5 max-w-[500px] flex flex-col bg-white shadow-xl items-center z-10 rounded-lg px-8 py-14 '>
          <HiOutlineXMark className='fixed top-2 left-2 text-[30px] hover:bg-black-100/20 rounded-full duration-200 p-1' onClick={closeModal} />
          <img src={logo} alt="logo" className='w-[180px]' />
--- frontend/src/components/ErrorModal.jsx
+++ frontend/src/components/ErrorModal.jsx
@@ -9,7 +9,7 @@ const ErrorModal = ({open, closeError, error}) => {
 
   return (
     <div className='bg-black-100/50 fixed w-full h-full z-10 ' onClick={closeError}>
-     <div className=' fixed top-[5%] left-1/2 -translate-x-1/2 w-2/3 max-w-[400px] flex bg-red/70 shadow-xl items-center gap-2 z-10 rounded-lg p-4 ' onClick={(e) => e.stopPropagation()}>
+     <div className=' fixed top-[2%] left-1/2 -translate-x-1/2 w-2/3 max-w-[400px] flex bg-red/70 shadow-xl items-center gap-2 z-10 rounded-lg p-4 ' onClick={(e) => e.stopPropagation()}>
          <HiOutlineXMark className='text-[30px] hover:bg-black-100/20 rounded-full duration-200 p-1' onClick={closeError} />
          <h1 className='font-semibold'>{error}</h1>
      </div>
