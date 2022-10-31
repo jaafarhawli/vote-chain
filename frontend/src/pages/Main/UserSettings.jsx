57a5b41cf23522a26e98c1d4c4149d29eb56e74c
JaafarHawli <jaafarhawle@gmail.com>
2022-10-31 12:11:05 +0200

    limking: linked update settings api to frontend

--- frontend/src/pages/Main/UserSettings.jsx
+++ frontend/src/pages/Main/UserSettings.jsx
@@ -1,15 +1,39 @@
 import React, {useState} from 'react';
 import MainHeader from '../../components/MainHeader';
+import axios from '../../api/axios';
 
 const UserSettings = () => {
 
-  const [firstname, setFirstname] = useState();
-  const [lastname, setLastname] = useState();
-  const [email, setEmail] = useState();
+  const [firstname, setFirstname] = useState(localStorage.firstname);
+  const [lastname, setLastname] = useState(localStorage.lastname);
+  const [email, setEmail] = useState(localStorage.email);
   const [oldPassword, setOldPassword] = useState();
   const [password, setPassword] = useState();
   const [confirm, setConfirm] = useState();
   const [error, setError] = useState('');
+
+  const saveInfo = async() => {
+    const form = {
+        id: localStorage.id,
+        first_name: firstname,
+        last_name: lastname,
+        email: email,
+    }
+    try {
+      const accountInfo = await axios.put('user/account', form, {
+        headers: {
+          Authorization: `bearer ${localStorage.token}`
+        }
+      });
+      localStorage.setItem('firstname', firstname);
+      localStorage.setItem('lastname', lastname);
+      localStorage.setItem('email', email);
+      console.log(accountInfo);
+    } catch (error) {
+      setError(error.message);
+      console.log(error);
+    }
+}
 
   return (
     <div>
@@ -18,17 +42,17 @@ const UserSettings = () => {
           <h1 className='text-[28px] font-semibold text-purple-100'>Account Info</h1>
             <label>
                 <p className='font-medium'>First Name</p>
-                <input type="text" className=' border-[1px] border-black-200' value={localStorage.firstname} onChange={e => setFirstname(e.target.value)}/>
+                <input type="text" className=' border-[1px] border-black-200' defaultValue={localStorage.firstname} onChange={e => setFirstname(e.target.value)}/>
             </label>
             <label>
                 <p className='font-medium'>Last Name</p>
-                <input type="text" className=' border-[1px] border-black-200' value={localStorage.lastname}  onChange={e => setLastname(e.target.value)}/>
+                <input type="text" className=' border-[1px] border-black-200' defaultValue={localStorage.lastname}  onChange={e => setLastname(e.target.value)}/>
             </label>
           <label>
               <p className='font-medium'>Email</p>
-              <input type="email" className=' border-[1px] border-black-200' value={localStorage.email}  onChange={e => setEmail(e.target.value)}/>
+              <input type="email" className=' border-[1px] border-black-200' defaultValue={localStorage.email}  onChange={e => setEmail(e.target.value)}/>
           </label>
-          <button>Save changes</button>
+          <button type='button' onClick={saveInfo}>Save changes</button>
         </form>
         <form className='my-12 lg:w-[600px] w-[400px] flex flex-col gap-5 lg:px-28 md:px-10 px-4'>
           <h1 className='text-[28px] font-semibold text-purple-100'>Change Password</h1>
