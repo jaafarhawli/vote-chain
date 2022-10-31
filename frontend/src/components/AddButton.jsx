0f02bc8cacf911b37b32629547c55694b9ba591d
JaafarHawli <jaafarhawle@gmail.com>
2022-10-31 08:49:01 +0200

    frontend: modified AddButton component

--- frontend/src/components/AddButton.jsx
+++ frontend/src/components/AddButton.jsx
@@ -4,7 +4,7 @@ import {FaRegPlusSquare} from 'react-icons/fa';
 const AddButton = (props) => {
   return (
     <div>
-      <button>
+      <button className={props.class} onClick={props.click}>
           <div className='flex items-center gap-1 justify-center'>
               {props.text} <FaRegPlusSquare className='text-[18px]' />          
           </div>
