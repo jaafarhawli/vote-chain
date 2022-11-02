7092e10610a05ff9db4c25b709d46fa068d100ed
JaafarHawli <jaafarhawle@gmail.com>
2022-11-02 03:36:51 +0200

    frontend: added functionality to admin election title buttons

--- frontend/src/components/AdminTitle.jsx
+++ frontend/src/components/AdminTitle.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import {AiFillHome} from 'react-icons/ai';
+import { useNavigate } from 'react-router-dom';
 
 const AdminTitle = () => {
-  return (
+
+    const navigate = useNavigate();
+  
+    return (
     <div className='fixed pl-[300px] w-full'>
       <div className='w-full bg-purple-100 py-6 flex justify-between px-8 items-center'>
         <div className='text-white w-3/5 flex flex-col justify-center'>
@@ -17,8 +21,8 @@ const AdminTitle = () => {
               </div>
         </div>
         <div className='flex gap-8 items-center'>
-            <AiFillHome className='text-white text-[30px] hover:text-white/80 duration-150' />
-            <button className='bg-white/20 px-6 py-6'>{localStorage.firstname} {localStorage.lastname}</button>
+            <AiFillHome className='text-white text-[30px] hover:text-white/80 duration-150' onClick={() => navigate('/main')} />
+            <button className='bg-white/20 px-6 py-6' onClick={() => navigate('/main/settings')}>{localStorage.firstname} {localStorage.lastname}</button>
         </div>
       </div>
     </div>
