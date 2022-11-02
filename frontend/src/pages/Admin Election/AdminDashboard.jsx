0160149dbe9c3aeb3ff68fd1ad0efb3be28d6f2c
JaafarHawli <jaafarhawle@gmail.com>
2022-11-02 06:58:16 +0200

    frontend: added election url to dashboard

--- frontend/src/pages/Admin Election/AdminDashboard.jsx
+++ frontend/src/pages/Admin Election/AdminDashboard.jsx
@@ -22,6 +22,11 @@ const AdminDashboard = () => {
           <input type="text" value={localStorage.election_code} readOnly={true} className='text-[20px] bg-black-100/5 w-1/2 py-5 px-4  border-black-200 border-2 shadow-inner rounded-md' />
           <TbCopy className='text-black-200 p-2 border-black-200 border-2 w-[44px] h-[44px] rounded-lg' onClick={() => copyTextToClipboard(localStorage.election_code)}/>
       </div>
+      <h2 className='text-purple-100 mt-8 text-[22px] font-semibold'>Election URL</h2>
+      <div className='flex gap-4 items-center mt-2'>
+          <input type="text" value={'-'} readOnly={true} className='text-[20px] bg-black-100/5 w-1/2 py-5 px-4  border-black-200 border-2 shadow-inner rounded-md' />
+          <TbCopy className='text-black-200 p-2 border-black-200 border-2 w-[44px] h-[44px] rounded-lg' onClick={() => copyTextToClipboard('-')}/>
+      </div>
       <ToastContainer autoClose={1000} hideProgressBar={true} position="bottom-center" />
     </div>
   );
