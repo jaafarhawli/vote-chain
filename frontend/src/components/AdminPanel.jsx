437f77530b896e28eb550e55c854010cd0115f3b
JaafarHawli <jaafarhawle@gmail.com>
2022-11-02 05:06:52 +0200

    frontend: changed dashboard icon

--- frontend/src/components/AdminPanel.jsx
+++ frontend/src/components/AdminPanel.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
 import logo from '../assets/VOTE CHAIN-logo-white-horizantal.png';
-import {AiFillHome} from 'react-icons/ai';
+import {RiDashboardFill} from 'react-icons/ri';
 import {HiUserGroup} from 'react-icons/hi';
 import {FaUserTie, FaUserCog, FaUser} from 'react-icons/fa';
 import {MdSettings} from 'react-icons/md';
@@ -10,11 +10,11 @@ import {TbCloudUpload} from 'react-icons/tb';
 const AdminPanel = () => {
   return (
     <div className='fixed top-0 left-0 w-[300px] h-full bg-purple-500 flex flex-col'>
-      <img src={logo} alt="" className='w-2/3 self-center pt-4' />
-      <h1 className='self-center text-white bg-purple-300/20 w-2/3 text-center mt-6 py-1 font-semibold select-none'>Admin</h1>
+      <img src={logo} alt="" className='w-3/5 self-center pt-8' />
+      <h1 className='self-center text-white bg-purple-300/20 w-3/5 text-center mt-6 py-1 font-semibold select-none'>Admin</h1>
       <ul className='flex flex-col w-full mt-6'>
         <NavLink to='dashboard' className='flex items-center gap-3 text-white/60 text-xl w-full hover:bg-purple-300/40 duration-200 py-4 pl-4' activeclassname='active'>
-            <AiFillHome />
+            <RiDashboardFill />
             Dashboard
         </NavLink>
         <NavLink to='moderators' className='flex items-center gap-3 text-white/60 text-xl w-full hover:bg-purple-300/40 duration-200 py-4 pl-4' activeclassname='active'>
