b48a8e067797101cd72f0dc55522f2a7e37c3451
JaafarHawli <jaafarhawle@gmail.com>
2022-11-02 13:08:42 +0200

    frontend: added empty state to admin parties route

--- frontend/src/pages/Admin Election/AdminParties.jsx
+++ frontend/src/pages/Admin Election/AdminParties.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useQuery} from '@tanstack/react-query';
 import axios from '../../api/axios';
+import AddButton from '../../components/AddButton';
 
 const AdminParties = () => {
 
@@ -11,6 +12,23 @@ const AdminParties = () => {
                     }
                   }).then((res) => res.data);
     })
+
+    if(data?.length === 0)
+    return (
+        <>
+        <div className='pl-[330px] pt-[150px] pr-6'>
+            <h1 className='text-[28px] font-bold'>Parties</h1>
+            <div className='flex flex-col w-full items-center gap-4 mt-[150px]'>
+                <div className='text-center'>
+                    <h2 className='text-[24px] font-semibold text-black-200'>No Parties</h2>
+                    <p className='text-[18px] w-64 mt-1'>You don’t have any party, add one now!</p>
+                </div>
+                <AddButton text={"Add party"} />
+            </div>
+        </div>
+        </>
+    );
+
 
   return (
     <div>
