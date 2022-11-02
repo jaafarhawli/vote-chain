c66f1fa92fb57bd2403cda3c868eda9693007ec7
JaafarHawli <jaafarhawle@gmail.com>
2022-11-02 03:31:36 +0200

    linking: linked view election as admin api to frontend

--- frontend/src/pages/Admin Election/AdminElection.jsx
+++ frontend/src/pages/Admin Election/AdminElection.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
+import {useQuery} from '@tanstack/react-query';
+import axios from '../../api/axios';
 import {Route, Routes} from 'react-router-dom';
 import AdminPanel from '../../components/AdminPanel';
+import AdminTitle from '../../components/AdminTitle';
 import AdminDashboard from './AdminDashboard';
 
 const AdminElection = () => {
+
+    const {data} = useQuery(["election"], async () => {
+        return axios.get(`user/election/${localStorage.id}/${localStorage.election_id}`, {
+                    headers: {
+                      Authorization: `bearer ${localStorage.token}`
+                    }
+                  }).then((res) => res.data);
+    })
+
+    localStorage.setItem('election_title', data?.title);
+    localStorage.setItem('start_time', data?.start_time);
+    localStorage.setItem('end_time', data?.end_time);
+
   return (
     <div>
       <AdminPanel />
+      <AdminTitle />
       <Routes>
           <Route path='/dashboard' element={<AdminDashboard />} />
       </Routes>
