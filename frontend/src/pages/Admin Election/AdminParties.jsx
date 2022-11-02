8f7e5c2932f56e2986e5127d69a7aab712ac8d4b
JaafarHawli <jaafarhawle@gmail.com>
2022-11-02 13:03:55 +0200

    linking: linked view parties api to frontend

--- frontend/src/pages/Admin Election/AdminParties.jsx
+++ frontend/src/pages/Admin Election/AdminParties.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
+import {useQuery} from '@tanstack/react-query';
+import axios from '../../api/axios';
 
 const AdminParties = () => {
+
+    const {data} = useQuery([], async () => {
+        return axios.get(`user/parties/${localStorage.election_id}`, {
+                    headers: {
+                      Authorization: `bearer ${localStorage.token}`
+                    }
+                  }).then((res) => res.data);
+    })
+
   return (
     <div>
       
