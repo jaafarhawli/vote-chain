f602058ff7eed3fc29e557fc545c235675069a06
JaafarHawli <jaafarhawle@gmail.com>
2022-11-02 06:53:18 +0200

    frontend: added toast notification to copy icon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Main from './pages/Main/Main';
 
+
 function App() {
 
   const client = new QueryClient();
--- frontend/src/pages/Admin Election/AdminDashboard.jsx
+++ frontend/src/pages/Admin Election/AdminDashboard.jsx
@@ -1,11 +1,13 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {TbCopy} from 'react-icons/tb';
-
+import { ToastContainer, toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const AdminDashboard = () => {
 
     const copyTextToClipboard = async (text) => {
         if ('clipboard' in navigator) {
+          toast("Copied to clipboard");
           return await navigator.clipboard.writeText(text);
         } else {
           return document.execCommand('copy', true, text);
@@ -20,6 +22,7 @@ const AdminDashboard = () => {
           <input type="text" value={localStorage.election_code} readOnly={true} className='text-[20px] bg-black-100/5 w-1/2 py-5 px-4  border-black-200 border-2 shadow-inner rounded-md' />
           <TbCopy className='text-black-200 p-2 border-black-200 border-2 w-[44px] h-[44px] rounded-lg' onClick={() => copyTextToClipboard(localStorage.election_code)}/>
       </div>
+      <ToastContainer autoClose={1000} hideProgressBar={true} position="bottom-center" />
     </div>
   );
 }
