ed025fde5a262721e4b0af7c08ad62196062c814
JaafarHawli <jaafarhawle@gmail.com>
2022-11-03 10:49:47 +0200

    frontend: added upload image to add candidate function

--- frontend/src/components/Modals/AddCandidate.jsx
+++ frontend/src/components/Modals/AddCandidate.jsx
@@ -13,6 +13,7 @@ const AddCandidate = ({open, closeModal}) => {
     const [errorModal, setErrorModal] = useState(false);
     const [error, setError] = useState('');
     const [image, setImage] = useState();
+    const [isError, setIsError] = useState(true);
     const imageRef = useRef(null);
     
     const addCandidate = async () => {
@@ -28,40 +29,43 @@ const AddCandidate = ({open, closeModal}) => {
                   Authorization: `bearer ${localStorage.token}`
                 }
               });
-              localStorage.setItem('candidate_id', data.data._id)
-              closeModal();
+              localStorage.setItem('candidate_id', data.data._id);
+              if(image) {
+                const formData = new FormData();
+
+                formData.append('candidate_id',localStorage.candidate_id);
+                formData.append('party_id', localStorage.party_id);
+                formData.append('candidateImg',image, image.name);
+
+                try {
+                    await axios.post('user/image', formData, {
+                       headers: {
+                         Authorization: `bearer ${localStorage.token}`
+                       }
+                     });
+      
+                   } catch (error) {
+                       setError(error.message);
+                       setIsError(true);
+                       setErrorModal(true);
+                     console.log(error);
+                   }
+              }
+              setIsError(false);
+              setError("Candidate added");
+              setErrorModal(true);
             } catch (error) {
                 setError(error.message);
+                setIsError(true);
                 setErrorModal(true);
               console.log(error);
             }
         
     }
 
-    const uploadImage = async () => {
-        const formData = new FormData();
-
-        formData.append('candidate_id',localStorage.candidate_id);
-        formData.append('party_id', localStorage.party_id);
-        formData.append('candidateImg',image, image.name);
-        
-        try {
-            const data = await axios.post('user/image', formData, {
-               headers: {
-                 Authorization: `bearer ${localStorage.token}`
-               }
-             });
-             console.log(data);
-           } catch (error) {
-               setError(error.message);
-               setErrorModal(true);
-             console.log(error);
-           }
-    }
 
     const handleClick = event => {
         imageRef.current.click();
-        setImage(event.current.files[0]);
       };
 
   
@@ -70,7 +74,7 @@ const AddCandidate = ({open, closeModal}) => {
   
     return (
       <div className='bg-black-100/50 fixed w-full h-full z-10 '>
-          <SuccessModal open={errorModal} message={error} error={true} closeError={() => setErrorModal(false)} />
+          <SuccessModal open={errorModal} message={error} error={isError} closeError={() => setErrorModal(false)} />
        <div className=' fixed top-[50%] left-1/2 -translate-x-1/2 -translate-y-1/2 md:w-2/3 w-4/5 max-w-[500px] flex flex-col bg-white shadow-xl items-center z-10 rounded-lg px-8 py-14 '>
            <HiOutlineXMark className='fixed top-2 left-2 text-[30px] hover:bg-black-100/20 rounded-full duration-200 p-1' onClick={closeModal} />
            <img src={logo} alt="logo" className='w-[180px]' />
