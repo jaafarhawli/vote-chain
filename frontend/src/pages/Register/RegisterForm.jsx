eab328136be042e7add2f8d18cffdd06d281fbf3
JaafarHawli <jaafarhawle@gmail.com>
2022-11-03 08:21:35 +0200

    frontend: added disabled state to register form button

--- frontend/src/pages/Register/RegisterForm.jsx
+++ frontend/src/pages/Register/RegisterForm.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { useNavigate } from 'react-router-dom';
 import logo from '../../assets/VOTE CHAIN-logo-black.png';
 import axios from '../../api/axios';
@@ -9,12 +9,13 @@ const RegisterForm = () => {
 
   const navigate = useNavigate();
 
-  const [firstname, setFirstname] = useState();
-  const [lastname, setLastname] = useState();
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
-  const [confirm, setConfirm] = useState();
+  const [firstname, setFirstname] = useState('');
+  const [lastname, setLastname] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirm, setConfirm] = useState('');
   const [error, setError] = useState('');
+  const [disabled, setDisabled] = useState(true);
 
   const handleSubmit = async() => {
       if(confirm!==password) {
@@ -42,6 +43,13 @@ const RegisterForm = () => {
       }
   }
 
+  useEffect(() => {
+    if(email==='' || password.length<8 || confirm.length<8 || firstname==='' || lastname==='')
+    setDisabled(true)
+    else
+    setDisabled(false)
+  }, [email, password, confirm, firstname, lastname]);
+
   return (
     <div className='bg-gradient-to-br from-white/70 to-white/30 w-[450px]  rounded-xl flex flex-col items-center p-6 pb-10 backdrop-blur-2xl shadow-2xl before:absolute before:bg-white/[15%] before:inset-0 before:rotate-[-5deg] before:-z-[1] before:rounded-xl'>
       <img src={logo} alt="logo" className='w-[180px]' />
@@ -56,7 +64,7 @@ const RegisterForm = () => {
           <FormInput type="email" className='border-0' onChange={e => setEmail(e.target.value)}>Email</FormInput> 
           <FormInput type="password" className='border-0' onChange={e => setPassword(e.target.value)}>Password</FormInput> 
           <FormInput type="password" className='border-0' onChange={e => setConfirm(e.target.value)}>Confirm Password</FormInput> 
-          <Button className='bg-cyan' onClick={handleSubmit} >Get Started</Button>
+          <Button className='bg-cyan' onClick={handleSubmit} disabled={disabled} >Get Started</Button>
       </form> 
       <p className='mt-4 text-[16px]'>Already have an account? <span className='font-semibold text-purple-100 select-none hover:underline' onClick={() => navigate('/login')}>Log in</span></p>
     </div>
