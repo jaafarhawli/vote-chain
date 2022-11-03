68160f4506e6435681b3a56e94bcf556d9de2563
JaafarHawli <jaafarhawle@gmail.com>
2022-11-03 06:26:27 +0200

    frontend: added change password function to it's modal

--- frontend/src/components/Modals/ChangePassword.jsx
+++ frontend/src/components/Modals/ChangePassword.jsx
@@ -4,6 +4,7 @@ import axios from '../../api/axios';
 import logo from '../../assets/VOTE CHAIN-logo-black.png';
 import Button from '../Reusable/Button';
 import ErrorModal from './ErrorModal';
+import SuccessModal from './SuccessModal';
 import FormInput from '../Reusable/FormInput';
 
 const ChangePassword = ({open, closeModal}) => {
@@ -15,12 +16,42 @@ const ChangePassword = ({open, closeModal}) => {
     const [error, setError] = useState('');
     const [successModal, setSuccessModal] = useState(false);
 
+    const changePassword = async() => {
+
+        if(confirm!==password) {
+            setError('Passwords didnt match');
+            setErrorModal(true);
+            return;
+        }
+    
+        const form = {
+            id: localStorage.id,
+            old_password: oldPassword,
+            password: password,
+        }
+        try {
+          await axios.put('user/password', form, {
+            headers: {
+              Authorization: `bearer ${localStorage.token}`
+            }
+          });
+     
+          setSuccessModal(true);
+    
+        } catch (error) {
+          setError(error.message);
+          setErrorModal(true);
+          console.log(error);
+        }
+    }
+
     if(!open)
     return null;
 
   return (
     <div className='bg-black-100/50 fixed w-full h-full z-10 '>
         <ErrorModal open={errorModal} error={error} closeError={() => setErrorModal(false)} />
+        <SuccessModal open={successModal} closeSuccess={() => setSuccessModal(false)} />
      <div className=' fixed top-[50%] left-1/2 -translate-x-1/2 -translate-y-1/2 md:w-2/3 w-4/5 max-w-[500px] flex flex-col bg-white shadow-xl items-center z-10 rounded-lg px-8 py-14 '>
          <HiOutlineXMark className='fixed top-2 left-2 text-[30px] hover:bg-black-100/20 rounded-full duration-200 p-1' onClick={closeModal} />
          <img src={logo} alt="logo" className='w-[180px]' />
