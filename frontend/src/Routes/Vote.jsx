e8892689079725929c6f2f23a5b06cd9353d7493
JaafarHawli <jaafarhawle@gmail.com>
2022-11-04 19:50:34 +0200

    frontend: added vote login page

--- frontend/src/Routes/Vote.jsx
+++ frontend/src/Routes/Vote.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
 import VoteLanding from '../pages/Vote/VoteLanding';
+import VoteLogin from '../pages/Vote/VoteLogin';
 
 const Vote = () => {
   return (
     <div>
       <Routes>
             <Route path='/' element={<VoteLanding />} />         
+            <Route path='/login' element={<VoteLogin />} />         
       </Routes>
     </div>
   );
---
+++ frontend/src/pages/Vote/VoteLogin.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const VoteLogin = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default VoteLogin;
