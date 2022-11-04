a6cb6fcc4362f36f04300ee9cab6df4281c804e8
JaafarHawli <jaafarhawle@gmail.com>
2022-11-05 01:36:30 +0200

    frontend: added election card to vote select page

--- frontend/src/pages/Vote/VoteSelect.jsx
+++ frontend/src/pages/Vote/VoteSelect.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import ElectionCard from '../../components/Reusable/ElectionCard';
 import VoteHeader from './VoteHeader';
 
 const VoteSelect = () => {
   return (
     <div>
       <VoteHeader />
+      <div className='lg:px-28 md:px-10 px-4'>
+        <ElectionCard id={localStorage.election_id} title={localStorage.election_title} start_time={localStorage.election_start} end_time={localStorage.election_end} className='!bg-black-300 mt-4' title_size='text-[28px]' date_size='!text-[15px]' />
+      </div>
     </div>
   );
 }
