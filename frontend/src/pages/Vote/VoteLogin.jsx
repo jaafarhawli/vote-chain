43b39af8d78e2f9f3c21beb68901d41ba2cf517c
JaafarHawli <jaafarhawle@gmail.com>
2022-11-04 19:52:53 +0200

    frontend: styled vote login page

--- frontend/src/pages/Vote/VoteLogin.jsx
+++ frontend/src/pages/Vote/VoteLogin.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import banner from '../../assets/banner.png';
 
 const VoteLogin = () => {
   return (
-    <div>
-      
+    <div className='bg-gradient-to-r from-purple-100 to-purple-200  flex w-full h-screen justify-center items-center md:justify-between'>
+      <div className='flex md:w-1/2 justify-center items-center'>
+
+      </div>
+      <img src={banner} alt="/" className='hidden md:flex w-1/2 object-cover h-full' />
     </div>
   );
 }
