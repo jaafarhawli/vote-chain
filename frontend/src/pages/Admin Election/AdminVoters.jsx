81c739a70167cec0f6c9b7ccbe7cc04df630ff56
JaafarHawli <jaafarhawle@gmail.com>
2022-11-04 05:35:51 +0200

    linking: linked view voters api to frontend

--- frontend/src/pages/Admin Election/AdminVoters.jsx
+++ frontend/src/pages/Admin Election/AdminVoters.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import {useQuery} from '@tanstack/react-query';
+import axios from '../../api/axios';
 
 const AdminVoters = () => {
+
+    const {data} = useQuery([], async () => {
+        return axios.get(`user/voters/${localStorage.election_id}`, {
+                    headers: {
+                      Authorization: `bearer ${localStorage.token}`
+                    }
+                  }).then((res) => res.data);
+    })
+
+
   return (
-    <div>
-      
-    </div>
+    <>
+    </>
   );
 }
 
