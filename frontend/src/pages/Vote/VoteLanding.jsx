9ec18b7e53843de1b7036bd5099ec40bc0013a10
JaafarHawli <jaafarhawle@gmail.com>
2022-11-04 19:45:45 +0200

    frontend: added functionality to join election button

--- frontend/src/pages/Vote/VoteLanding.jsx
+++ frontend/src/pages/Vote/VoteLanding.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Button from '../../components/Reusable/Button';
 import logo from '../../assets/VOTE CHAIN-logo-white-horizantal.png';
+import { useNavigate } from 'react-router-dom';
 import landingsm from '../../assets/landing-sm.svg';
 import landinglg from '../../assets/landing-lg.svg';
 
 const VoteLanding = () => {
 
-    
+    const navigate = useNavigate();
 
   return (
     <div className='w-full h-screen bg-gradient-to-b from-black-100 to-black-200 flex flex-col'>
@@ -17,7 +18,7 @@ const VoteLanding = () => {
         <div className='flex flex-col justify-center sm:items-start items-center text-center sm:text-left'>
             <h1 className='text-4xl text-white md:text-5xl lg:text-6xl pt-12 md:pt-8'><span className='text-purple-100 font-semibold'>Blockchain-based</span><br/>online voting system </h1>
             <p className='text-white pt-6'>Join election and vote for your candidate</p>
-            <Button className={'mt-6 max-w-[300px] gap-2'} >Join election</Button>
+            <Button className={'mt-6 max-w-[300px] gap-2'} onClick={() => navigate('login')} >Join election</Button>
         </div>
         <img src={landingsm} alt="talents" className="sm:w-[45%] md:hidden flex mt-5 w-[300px]" />
         <img src={landinglg} alt="talents" className="w-[45%] hidden md:flex mt-5" />
