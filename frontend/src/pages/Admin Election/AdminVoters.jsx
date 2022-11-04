4eaf5c146c5ad754f1bb83be3aeb42b5c92e4795
JaafarHawli <jaafarhawle@gmail.com>
2022-11-04 05:44:19 +0200

    frontend: added non empty state to admin voters page

--- frontend/src/pages/Admin Election/AdminVoters.jsx
+++ frontend/src/pages/Admin Election/AdminVoters.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useQuery} from '@tanstack/react-query';
 import axios from '../../api/axios';
 import EmptyState from '../../components/Reusable/EmptyState';
+import Button from '../../components/Reusable/Button';
+import Table from '../../components/Reusable/Table';
 
 const AdminVoters = () => {
+
+    const [search, setSearch] = useState('');
 
     const {data} = useQuery([], async () => {
         return axios.get(`user/voters/${localStorage.election_id}`, {
@@ -21,7 +25,17 @@ const AdminVoters = () => {
         <h1 className='text-[28px] font-bold'>Voters</h1>
         <EmptyState title={'No Voters'} button={'Add voter'}>You don’t have any voters, add one now!</EmptyState>
     </div>
-    : null
+    : 
+    <>
+    <div className='pl-[330px] pt-[150px] pr-6'>
+        <div className='flex justify-between items-center w-full'>
+          <h1 className='text-[28px] font-bold'>Parties</h1>
+          <Button add={true}>Add Party</Button>
+        </div>
+        <input type="search" className='border-2 border-[#dddddd] w-1/3 rounded-md mt-4' placeholder='Search moderator by email' onChange={e => setSearch(e.target.value)} />
+        <Table data={data} voter={true} />
+    </div>
+    </>
     }     
     </>
   );
