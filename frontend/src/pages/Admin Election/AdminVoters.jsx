ac025b4252e8f549e1848ef0f3f7a64f1c6cd27f
JaafarHawli <jaafarhawle@gmail.com>
2022-11-04 06:36:47 +0200

    frontend: added confirm modal to admin voters

--- frontend/src/pages/Admin Election/AdminVoters.jsx
+++ frontend/src/pages/Admin Election/AdminVoters.jsx
@@ -5,12 +5,14 @@ import EmptyState from '../../components/Reusable/EmptyState';
 import Button from '../../components/Reusable/Button';
 import Table from '../../components/Reusable/Table';
 import AddVoter from '../../components/Modals/AddVoter';
+import ConfirmModal from '../../components/Modals/ConfirmModal';
 
 const AdminVoters = () => {
 
     const [search, setSearch] = useState('');
     const [voterModal, setVoterModal] = useState(false);
     const [refetch, setRefetch] = useState(true);
+    const [confirmModal, setConfirmModal] = useState(false);
 
     const closeModal = () => {
         setVoterModal(false)
@@ -20,6 +22,21 @@ const AdminVoters = () => {
     const openModal = () => {
       setVoterModal(true);
       document.body.style.overflow = 'hidden';
+    }
+
+    const openConfirmModal = (id) => {
+        setConfirmModal(true);
+        localStorage.setItem('voter_id', id)
+        document.body.style.overflow = 'hidden';
+      }
+
+    const closeConfirm = () => {
+      setConfirmModal(false)
+      document.body.style.overflow = 'unset';
+    }
+
+    const deleteVoter = () => {
+        console.log('delete');
     }
 
     const {data} = useQuery([refetch], async () => {
@@ -49,6 +66,7 @@ const AdminVoters = () => {
     </>
     : 
     <>
+    <ConfirmModal  open={confirmModal} closeModal={closeConfirm} click={deleteVoter} text={"Are you sure you want to delete this voter?"} />
     <AddVoter open={voterModal} closeModal={closeModal}  refetch={() => setRefetch(!refetch)} />
     <div className='pl-[330px] pt-[150px] pr-6'>
         <div className='flex justify-between items-center w-full'>
@@ -57,7 +75,7 @@ const AdminVoters = () => {
         </div>
         <input type="search" className='border-2 border-[#dddddd] w-1/3 rounded-md mt-4' placeholder='Search voter by email' onChange={e => setSearch(e.target.value)} />
         {filteredData?.length===0 ? <EmptyState title={'No Voters'}>You don’t have any voters</EmptyState> : 
-        <Table data={filteredData} voter={true} />
+        <Table data={filteredData} voter={true} remove={(id) => openConfirmModal(id)} />
         }
     </div>
     </>
