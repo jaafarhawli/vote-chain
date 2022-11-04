028e34380eeb6befcf1059a8836b223065c4090b
JaafarHawli <jaafarhawle@gmail.com>
2022-11-05 01:06:38 +0200

    frontend: added footer to vote main page

--- frontend/src/pages/Vote/VoteMain.jsx
+++ frontend/src/pages/Vote/VoteMain.jsx
@@ -5,6 +5,7 @@ import landingsm from '../../assets/landing-sm.svg';
 import landinglg from '../../assets/landing-lg.svg';
 import curve from '../../assets/backgroundcurve.svg';
 import Button from '../../components/Reusable/Button';
+import LandingFooter from '../Landing/LandingFooter';
 
 
 const VoteMain = () => {
@@ -32,7 +33,7 @@ const VoteMain = () => {
       <img src={curve} alt='/' className='w-full invisible max-h-[110px]' />
     </div>
     <div className='flex flex-col justify-center items-center w-full h-[300px] bg-gradient-to-b from-purple-300 to-purple-400'>
-        <h1 className='text-[30px] font-bold mb-8'>Time Left For Election</h1>
+        <h1 className='text-[34px] font-bold mb-8'>Time Left For Election</h1>
         <div className='flex gap-8'>
             <div className='w-[90px] h-[90px] bg-purple-100'></div>
             <div className='w-[90px] h-[90px] bg-purple-100'></div>
@@ -40,6 +41,7 @@ const VoteMain = () => {
             <div className='w-[90px] h-[90px] bg-purple-100'></div>
         </div>
     </div>
+    <LandingFooter />
     </>
   );
 }
