b4efd6e36782d59d8d15b6d60e9317640e5a3738
JaafarHawli <jaafarhawle@gmail.com>
2022-11-06 03:27:50 +0200

    frontend: added timer for election to end

--- frontend/src/pages/Vote/VoteMain.jsx
+++ frontend/src/pages/Vote/VoteMain.jsx
@@ -9,40 +9,74 @@ import LandingFooter from '../Landing/LandingFooter';
 import Timer from '../../components/Reusable/Timer';
 
 
+
 const VoteMain = () => {
-
+  
   const navigate = useNavigate();
-
+  
   const [timerDays, setTimerDays] = useState();
   const [timerHours, setTimerHours] = useState();
   const [timerMinutes, setTimerMinutes] = useState();
   const [timerSeconds, setTimerSeconds] = useState();
+  const [live, setLive] = useState(false);
+  const [ended, setEnded] = useState();
+  
+  const startEndTimer = () => {
+    const endDate = new Date(localStorage.election_end).getTime();
+  
+    let interval = setInterval(() => {
+      const now = new Date().getTime();
+      let distance = endDate - now;
+      
+      const days = Math.floor(distance / (24 * 60 * 60 * 1000));
+      const hours = Math.floor(distance % (24 * 60 * 60 * 1000) / (60 * 60 * 1000));
+      const minutes = Math.floor(distance % (60 * 60 * 1000) / (60 * 1000));
+      const seconds = Math.floor(distance % (60 * 1000) / 1000);
+  
+      if(distance<0) {    
+          setEnded(true);
+          clearInterval(interval.current);
+        }
+        
+      else {
+        setTimerDays(days);
+        setTimerHours(hours);
+        setTimerMinutes(minutes);
+        setTimerSeconds(seconds);
+      }
+    })
+  }
 
   const startTimer = () => {
     const startDate = new Date(localStorage.election_start).getTime();
 
     let interval = setInterval(() => {
       const now = new Date().getTime();
-      const distance = startDate - now;
+      let distance = startDate - now;
       
       const days = Math.floor(distance / (24 * 60 * 60 * 1000));
       const hours = Math.floor(distance % (24 * 60 * 60 * 1000) / (60 * 60 * 1000));
       const minutes = Math.floor(distance % (60 * 60 * 1000) / (60 * 1000));
       const seconds = Math.floor(distance % (60 * 1000) / 1000);
 
-      if(distance<0)
-      clearInterval(interval.current);
+      if(distance<0) {
+          setLive(true);
+          clearInterval(interval.current);
+      }
       else {
-        setTimerDays(days)
-        setTimerHours(hours)
-        setTimerMinutes(minutes)
-        setTimerSeconds(seconds)
+        setTimerDays(days);
+        setTimerHours(hours);
+        setTimerMinutes(minutes);
+        setTimerSeconds(seconds);
       }
     })
   }
- 
+
 
   useEffect(() => {
+    if(live)
+    startEndTimer();
+    else
     startTimer();
   });
 
@@ -68,6 +102,21 @@ const VoteMain = () => {
       <img src={curve} alt='/' className='w-full invisible max-h-[110px]' />
     </div>
     <div className='flex flex-col justify-center items-center w-full h-[300px] bg-gradient-to-b from-purple-300 to-purple-400'>
+        {ended ?
+        <h1 className='text-[34px] font-bold mb-8'>Election has ended</h1>
+        :
+        live ?
+        <>
+        <h1 className='text-[34px] font-bold mb-8'>Time Left For Election To End</h1>
+        <div className='flex gap-8'>
+            <Timer type={"Days"}>{timerDays}</Timer>
+            <Timer type={"Hours"}>{timerHours}</Timer>
+            <Timer type={"Minutes"}>{timerMinutes}</Timer>
+            <Timer type={"Seconds"}>{timerSeconds}</Timer>
+        </div>
+        </>
+        :
+        <>
         <h1 className='text-[34px] font-bold mb-8'>Time Left For Election</h1>
         <div className='flex gap-8'>
             <Timer type={"Days"}>{timerDays}</Timer>
@@ -75,6 +124,7 @@ const VoteMain = () => {
             <Timer type={"Minutes"}>{timerMinutes}</Timer>
             <Timer type={"Seconds"}>{timerSeconds}</Timer>
         </div>
+        </>}
     </div>
     <LandingFooter />
     </>
