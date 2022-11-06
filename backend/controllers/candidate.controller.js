820c213dab9eb95f380ad6da793c54838f77c408
JaafarHawli <jaafarhawle@gmail.com>
2022-11-06 13:01:54 +0200

    backend: added candidate controller

---
+++ backend/controllers/candidate.controller.js
@@ -0,0 +1,7 @@
+const Election = require('../models/elections.model');
+const User = require('../models/users.model');
+const Party = require('../models/parties.model');
+
+module.exports = {
+    
+}
