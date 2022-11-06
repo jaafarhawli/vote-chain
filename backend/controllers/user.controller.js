b489a4c1f4a0fa1292298c79cc96cb9975768d75
JaafarHawli <jaafarhawle@gmail.com>
2022-11-06 12:29:11 +0200

    backend: moved create election function to election controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -17,38 +17,7 @@ const getUser = async (req, res) => {
     } 
 })};
 
-const createElection = async(req, res) => {
-    const {admin_id, title, start_time, end_time} = req.body;
-    let election_code = Math.random().toString(36).substring(2,7);
-    let code = Election.findOne({code: election_code});
 
-    while(code.length==1) {
-        election_code = Math.random().toString(36).substring(2,7);
-        code = Election.find({code: election_code});
-    }
-
-    try{
-        const election = new Election();
-        election.title = title;
-        election.start_time = start_time;
-        election.end_time = end_time;
-        election.code = election_code;
-        election.admin = admin_id;
-        election.launched = false;
-        await election.save();
-
-        const admin = await User.findById(admin_id);
-        admin.elections.push(election._id);
-        await admin.save();
-
-        res.status(200).json(election);
-
-    }catch(err){
-        res.status(400).json({
-            message: err.message
-        })
-    }
-}
 
 const viewElectionsAsAdmin = (req, res) => {
     const {id} = req.params;
@@ -549,7 +518,6 @@ const rejectRequest = async (req, res) => {
 
 module.exports = {
     getUser,
-    createElection,
     viewElectionsAsAdmin,
     viewElectionsAsModerator,
     editAccount,
