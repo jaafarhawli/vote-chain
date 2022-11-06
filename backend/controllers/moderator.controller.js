1c8e69eec9d72658ca3d8b17b7ab03fa69be6aa3
JaafarHawli <jaafarhawle@gmail.com>
2022-11-06 13:27:14 +0200

    backend: moved add moderator function to moderator controller

--- backend/controllers/moderator.controller.js
+++ backend/controllers/moderator.controller.js
@@ -2,10 +2,32 @@ const Party = require('../models/parties.model');
 const Election = require('../models/elections.model');
 const User = require('../models/users.model');
 
+const addModerator = async (req, res) => {
+    const {email, election_id, sender_email} = req.body;
+    User.findOne({email: email}, async (err, user) => {
+        if(err) 
+        return res.status(404).json("User not found");
+        if(!user)
+        return res.status(404).json("User not found");
+        if(user.elections.includes(election_id))
+        return res.status(400).json("Invalid request");
+        Election.findById(election_id, async (err, election) => {
+            if(err) 
+            return res.status(404).json("Election not found");
+            if(election.moderators.includes(user._id))
+            return res.status(400).json("This user is already a moderator to your election");
+            user.notifications.push({
+                user_email: sender_email,
+                election_id: election_id,
+                election_title: election.title
+            });
+            await user.save();
 
+            res.status(200).json({message: "Request sent successfully"});
+    });
+});
+}
 
 module.exports = {
-    addParty,
-    removeParty,
-    viewParties
+    addModerator
 }
--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -128,32 +128,6 @@ const viewElectionAsModerator = (req, res) => {
     });
 }
 
-const addModerator = async (req, res) => {
-    const {email, election_id, sender_email} = req.body;
-    User.findOne({email: email}, async (err, user) => {
-        if(err) 
-        return res.status(404).json("User not found");
-        if(!user)
-        return res.status(404).json("User not found");
-        if(user.elections.includes(election_id))
-        return res.status(400).json("Invalid request");
-        Election.findById(election_id, async (err, election) => {
-            if(err) 
-            return res.status(404).json("Election not found");
-            if(election.moderators.includes(user._id))
-            return res.status(400).json("This user is already a moderator to your election");
-            user.notifications.push({
-                user_email: sender_email,
-                election_id: election_id,
-                election_title: election.title
-            });
-            await user.save();
-
-            res.status(200).json({message: "Request sent successfully"});
-    });
-});
-}
-
 const removeModerator = async (req, res) => {
     const {moderator_id, election_id} = req.body;
     User.findById(moderator_id, (err, user) => {
