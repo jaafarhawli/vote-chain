2ee294af01dac3e2cd567a412ef62e992eeb28c5
JaafarHawli <jaafarhawle@gmail.com>
2022-11-06 09:22:17 +0200

    backend: added launched middleware

---
+++ backend/middlewares/launched.middleware.js
@@ -0,0 +1,16 @@
+const Election = require('../models/elections.model');
+
+const launchedMiddleware = async (req, res, next) => {
+    const {election_id} = req.body;
+    Election.findById(election_id, (err, election) => {
+        if(err)
+        return res.status(404).json({message:"Election not found"});
+        if(election.launched===false)
+        next();
+        else
+        return res.status(404).json({message:"Election is launched"});
+    })
+
+}
+
+module.exports = launchedMiddleware;
