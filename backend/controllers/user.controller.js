4698227c654b3fbe04c6e79cbe5c54f0b3356b96
JaafarHawli <jaafarhawle@gmail.com>
2022-11-07 02:33:24 +0200

    backend: modified user controller functions' outputs

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -8,9 +8,9 @@ const getUser = async (req, res) => {
         email: email
    }, async (err, user) => {
     if(err)
-    return res.status(404).json("User not found");
+    return res.status(404).json({message:"User not found"});
     else {
-        res.status(200).json(user);
+        res.status(200).json({data: user});
     } 
 })};
 
@@ -19,7 +19,7 @@ const editAccount = async (req, res) => {
 
     User.findById(id, (err) => {
         if(err) 
-        return res.status(400).json("Invalid input");
+        return res.status(400).json({message: "Invalid input"});
     });
     
     User.findByIdAndUpdate(id,{
@@ -28,8 +28,8 @@ const editAccount = async (req, res) => {
         email: data.email,
     }, async (err) => {
         if(err)
-        return res.status(400).json("Invalid input");
-        res.status(200).json("Account updated successfully");
+        return res.status(400).json({message:"Invalid input"});
+        res.status(200).json({message:"Account updated successfully"});
     });
 } 
 
@@ -41,27 +41,27 @@ const changePassword = async (req, res) => {
     if(!isMatch) return res.status(404).json({message: "Old password is invalid"});
     
     if(password.length<8) 
-    return res.status(400).json("Invalid password");
+    return res.status(400).json({message:"Invalid password"});
 
     hashed = await bcrypt.hash(password, 10);
     User.findByIdAndUpdate(id,{
         password: hashed
     }, async (err) => {
         if(err)
-        return res.status(400).json("Invalid password");
-        res.status(200).json("Password updated successfully");
+        return res.status(400).json({message:"Invalid password"});
+        res.status(200).json({message:"Password updated successfully"});
     });
 }
 
 const deleteAccount = (req, res) => {
     const {id} = req.body;
     Election.deleteMany({ admin: id }, function (err) {
-        if(err) console.log(err);
-        console.log("Successful deletion");
+        if(err) 
+        return res.status(400).json({ err });
       });
     Election.find({moderators: {"$in": [id]}}, (err, elections) => {
         if(err)
-        return res.status(400).json("Invalid request");
+        return res.status(400).json({ err });
         elections.forEach((election) => {
             const index = election.moderators.indexOf(id);
             console.log("index", index);
@@ -72,9 +72,9 @@ const deleteAccount = (req, res) => {
     
     User.findByIdAndDelete(id, (err) => {
         if(err)
-        return res.status(400).json("Invalid request");
+        return res.status(400).json({ err });
     });
-    return res.status(200).json('User deleted successfully');
+    return res.status(200).json({message: 'User deleted successfully'});
 }
 
 const viewNotifications = async (req, res) => {
@@ -90,16 +90,16 @@ const acceptRequest = async (req, res) => {
     const {user_id, election_id} = req.body;
     User.findById(user_id, async (err, user) => {
         if(err) 
-        return res.status(404).json("User not found");
+        return res.status(404).json({message:"User not found"});
         if(!user)
-        return res.status(404).json("User not found");
+        return res.status(404).json({message:"User not found"});
         if(user.elections.includes(election_id))
-        return res.status(400).json("Invalid request");
+        return res.status(400).json({message:"Invalid request"});
         Election.findById(election_id, async (err, election) => {
             if(err) 
-            return res.status(404).json("Election not found");
+            return res.status(404).json({message:"Election not found"});
             if(election.moderators.includes(user_id))
-            return res.status(400).json("You are already a moderator to this election");
+            return res.status(400).json({message:"You are already a moderator to this election"});
             user.moderator_for.push(election_id);
             await user.save();
 
