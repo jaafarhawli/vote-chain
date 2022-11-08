8e642c245b67f4cc8773423b4c7fa04ca381c763
JaafarHawli <jaafarhawle@gmail.com>
2022-11-08 21:17:34 +0200

    linking: linked verify email api to frontend

--- frontend/src/pages/Verification/EmailVerification.jsx
+++ frontend/src/pages/Verification/EmailVerification.jsx
@@ -1,19 +1,37 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {useParams} from 'react-router-dom';
 import logo from '../../assets/VOTE CHAIN-logo-black.png';
 import Button from '../../components/Reusable/Button';
+import axios from '../../api/axios';
 
 const EmailVerification = () => {
 
     const [message, setMessage] = useState("");
     const [error, setError] = useState(false);
 
+    const param = useParams();
+
+    useEffect(() => {
+      const verifyEmailUrl = async () => {
+          try {
+            const data = await axios.get(`/email/verify/${param.id}/${param.token}`);
+            console.log(data);
+              
+            } catch (error) {
+               setMessage(error);
+               setError(true); 
+              console.log(error);
+            }  
+          }
+          verifyEmailUrl();
+    }, [param]);
+
 
   return (
     <div className='flex flex-col w-screen h-screen bg-purple-300 justify-center items-center gap-3'>
       <img src={logo} alt="" className='w-[300px]' />
       <Button>Login</Button>
-      <p className={error ? 'text-center text-[24px] text-red' : 'text-center text-[24px] text-green'}>{message}</p>
+      {/* <p className={error ? 'text-center text-[24px] text-red' : 'text-center text-[24px] text-green'}>{message}</p> */}
     </div>
   );
 }
