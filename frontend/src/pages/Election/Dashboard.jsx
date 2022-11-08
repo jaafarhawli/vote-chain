7873eadd7143a33cabbaf149cdcfbf1682f84ac6
JaafarHawli <jaafarhawle@gmail.com>
2022-11-08 11:07:59 +0200

    frontend: added statistics component

--- frontend/src/pages/Election/Dashboard.jsx
+++ frontend/src/pages/Election/Dashboard.jsx
@@ -2,11 +2,7 @@ import React from 'react';
 import {TbCopy} from 'react-icons/tb';
 import { ToastContainer, toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import {useQuery} from '@tanstack/react-query';
-import axios from '../../api/axios';
-import { Doughnut } from 'react-chartjs-2';
-import { Bar } from 'react-chartjs-2';
-import { Chart as ChartJS } from 'chart.js/auto';
+import Statistics from '../../components/Reusable/Statistics';
 
 const Dashboard = () => {
 
@@ -17,64 +13,7 @@ const Dashboard = () => {
         } else {
           return document.execCommand('copy', true, text);
         }
-      }
-
-      const {data} = useQuery(["parties"], async () => {
-        return axios.get(`statistics/parties/${localStorage.election_id}`, {
-                    headers: {
-                      Authorization: `bearer ${localStorage.token}`
-                    }
-                  }).then((res) => res.data);
-      })
-      
-      const {data: candidates} = useQuery(["candidates"], async () => {
-        return axios.get(`statistics/candidates/${localStorage.election_id}`, {
-                    headers: {
-                      Authorization: `bearer ${localStorage.token}`
-                    }
-                  }).then((res) => res.data);
-      })
-      
-      const {data: parties} = useQuery(["partyCandidates"], async () => {
-        return axios.get(`statistics/party/candidates/${localStorage.election_id}`, {
-                    headers: {
-                      Authorization: `bearer ${localStorage.token}`
-                    }
-                  }).then((res) => res.data.result);
-      })
-
-    const partyStats = {
-      labels: data?.labels,
-      datasets: [{
-        data: data?.data,
-        backgroundColor: ["#4ba0f7", "#00B8FF", "#7685e4", "#9568c7", "#a847a1", "#ae1f74"]
-      }]
-    }
-
-
-    const candidatesNames = []
-    const candidatesScores = []
-    
-    // Divide sorted candidates into two arrays
-    if(candidates) 
-    for(const [key, value] of Object.entries(candidates.sorted)) {
-      candidatesNames.push(key);
-      candidatesScores.push(value);
-    }
-    
-    
-    const candidateStats = {
-      labels: candidatesNames.slice(0, 10),
-      datasets: [{
-        label: "Votes",
-        data: candidatesScores.slice(0, 10),
-        backgroundColor: ["#4ba0f7", "#00B8FF", "#7685e4", "#9568c7", "#a847a1", "#ae1f74"]
-      }]
-    }
-
-    const date = new Date();
-    const startDate = new Date(localStorage.election_start);
-    const live = date > startDate;
+      }  
 
     return (
     <div className='pl-[330px] pt-[150px] bg-purple-400 pr-8'>
@@ -89,82 +28,7 @@ const Dashboard = () => {
           <input type="text" value={'-'} readOnly={true} className='text-[20px] bg-black-100/5 w-1/2 py-5 px-4  border-black-200 border-2 shadow-inner rounded-md' />
           <TbCopy className='text-black-200 p-2 border-black-200 border-2 w-[44px] h-[44px] rounded-lg' onClick={() => copyTextToClipboard('-')}/>
       </div>
-    
-      {!live ?
-      <>
-      <div className='flex w-full gap-6'>
-        <div className='w-1/3 my-6 bg-white rounded-2xl shadow-xl p-6'>
-          <Doughnut data={partyStats} />
-        </div>
-        <div className='w-2/3 my-6 bg-white rounded-2xl shadow-xl p-6 flex align-baseline'>
-          <Bar data={candidateStats}
-                options={{
-                  scales: {
-                    y: {
-                      grid: {
-                        display: false
-                      }
-                    },
-                    x: {
-                      grid: {
-                        display: false
-                      }
-                    }
-                  },       
-                  elements: {
-                    bar: {
-                      borderWidth: 2,
-                    },
-                  },
-                  plugins: {
-                    legend: {
-                      position: 'right',
-                    },
-                    title: {
-                      display: true,
-                      text: 'Top Candidates',
-                    },
-                  },
-                }} />
-        </div>
-      </div>
-      <div className='grid grid-cols-2 gap-6 mb-6 w-full'>
-      {parties?.map((party) => (
-          <div className='w-full bg-white rounded-2xl shadow-xl p-6 flex align-baseline'>
-            <Bar data={{
-              labels: party.labels,
-              datasets: [{
-                label: party.party,
-                data: party.data,
-                backgroundColor: ["#4ba0f7", "#00B8FF", "#7685e4", "#9568c7", "#a847a1", "#ae1f74"]
-              }]}}
-              options={{
-                indexAxis: 'y',
-                elements: {
-                  bar: {
-                    borderWidth: 2,
-                  },
-                },
-                scales: {
-                  y: {
-                    grid: {
-                      display: false
-                    }
-                  },
-                  x: {
-                    grid: {
-                      display: false
-                    }
-                  }
-                }
-              }}
-             />
-          </div>
-      ))}
-      </div>
-      </>
-      :
-      null}
+      <Statistics />
       <ToastContainer autoClose={1000} hideProgressBar={true} position="bottom-center" />
     </div>
   );
