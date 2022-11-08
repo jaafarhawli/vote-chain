05a8a252a329ffcab583b42a73db2311fe2eb442
JaafarHawli <jaafarhawle@gmail.com>
2022-11-08 13:30:42 +0200

    frontend: modified panel

--- frontend/src/components/Reusable/Panel.jsx
+++ frontend/src/components/Reusable/Panel.jsx
@@ -11,7 +11,7 @@ const Panel = (props) => {
   return (
     <div className='fixed top-0 left-0 w-[300px] h-full bg-purple-500 flex flex-col'>
       <img src={logo} alt="" className='w-3/5 self-center pt-8' />
-      <h1 className='self-center text-white bg-purple-300/20 w-3/5 text-center mt-6 py-1 font-semibold select-none'>Admin</h1>
+      <h1 className='self-center text-white bg-purple-300/20 w-3/5 text-center mt-6 py-1 font-semibold select-none'>{props.admin ? "Admin" : "Moderator"}</h1>
       <ul className='flex flex-col w-full mt-6'>
         <NavLink to='dashboard' className='flex items-center gap-3 text-white/60 text-xl w-full hover:bg-purple-300/40 duration-200 py-4 pl-4 font-semibold' activeclassname='active'>
             <RiDashboardFill />
