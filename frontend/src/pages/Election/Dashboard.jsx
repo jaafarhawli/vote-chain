89c305a0b78742659efc0aa25138f2b1c01a3fc3
JaafarHawli <jaafarhawle@gmail.com>
2022-11-14 20:31:51 +0200

    frontend: added election appliance url to election dashboard

--- frontend/src/pages/Election/Dashboard.jsx
+++ frontend/src/pages/Election/Dashboard.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {TbCopy} from 'react-icons/tb';
 import { ToastContainer, toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Statistics from '../../components/Reusable/Statistics';
@@ -31,16 +30,15 @@ const Dashboard = () => {
     <div className='pl-[330px] pt-[150px] bg-purple-400 pr-8'>
       <h1 className='text-[28px] font-bold'>Election Dashboard</h1>
       <h2 className='text-purple-100 mt-8 text-[22px] font-semibold'>Election code</h2>
-      <div className='flex w-full items-stretch'>
+      <div className='flex w-full '>
         <div className='w-full'>
-          <div className='flex gap-4 items-center mt-2'>
-              <input type="text" value={localStorage.election_code} readOnly={true} className='text-[20px] bg-black-100/5 w-1/2 py-5 px-4  border-black-200 border-2 shadow-inner rounded-md' />
-              <TbCopy className='text-black-200 p-2 border-black-200 border-2 w-[44px] h-[44px] rounded-lg' onClick={() => copyTextToClipboard(localStorage.election_code)}/>
-          </div>
+          <CopyData value={localStorage.election_code} onClick={(value) => copyTextToClipboard(value)} />
           <h2 className='text-purple-100 mt-8 text-[22px] font-semibold'>Election URL</h2>
           <CopyData value={'-'} onClick={(value) => copyTextToClipboard(value)} />
+          <h2 className='text-purple-100 mt-8 text-[22px] font-semibold'>Election Appliance URL</h2>
+          <CopyData value={'-'} onClick={(value) => copyTextToClipboard(value)} />
         </div>
-        <ul className='bg-purple-300 w-[300px] rounded-2xl shadow-xl p-3 text-black-100 text-[24px] font-bold space-y-2 flex flex-col justify-center'>
+        <ul className='bg-purple-300 w-[300px] rounded-2xl shadow-xl p-3 text-black-100 text-[24px] font-bold space-y-2 flex flex-col justify-center max-h-[170px]'>
             <li>Voters: <span className='text-purple-100'>{data?.voters}</span> </li>
             <li>Parties: <span className='text-purple-100'>{data?.parties}</span> </li>
             <li>Candidates: <span className='text-purple-100'>{data?.candidates}</span> </li>
