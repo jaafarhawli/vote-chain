965e5a2e0077613a5c3c9021136c39d24f8ec2a9
JaafarHawli <jaafarhawle@gmail.com>
2022-11-16 15:09:31 +0200

    frontend: added store file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,37 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from 'react-redux';
-import {configureStore, combineReducers} from '@reduxjs/toolkit';
-import electionReducer from './redux/election';
-import {persistReducer, persistStore, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER,} from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
-import storage  from 'redux-persist/lib/storage';
+import {persistor, store} from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-const persistConfig =  {
-  key: 'root',
-  version: 1,
-  storage
-}
-
-const reducer = combineReducers({
-  election: electionReducer,
-})
-
-const persistedReducer = persistReducer(persistConfig, reducer);
-
-const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
-});
-
-let persistor = persistStore(store);
 
 root.render(
   <React.StrictMode>
