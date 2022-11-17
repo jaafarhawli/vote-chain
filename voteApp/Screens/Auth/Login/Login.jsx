d402302911abb724d25ba31dce6525fae0dd7bca
JaafarHawli <jaafarhawle@gmail.com>
2022-11-17 16:30:45 +0200

    frontend: added input states to login page

--- voteApp/Screens/Auth/Login/Login.jsx
+++ voteApp/Screens/Auth/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { View, Image } from 'react-native';
 import Input from '../../../components/input';
 import { images, colors } from '../../../constants';
@@ -8,15 +8,20 @@ import MyAppText from '../../../components/MyAppText';
 import { LinearGradient } from 'expo-linear-gradient';
 
 const Login = () => {
+
+  const [code, setCode] = useState('');
+  const [id, setId] = useState('');
+  const [key, setKey] = useState('');
+
   return (
     <LinearGradient style={styles.container} colors={[colors.primary[100], colors.white]}>
         <Image source={images.whiteLogoText} style={styles.image} fadeDuration={1000} />
         <MyAppText style={styles.title}>Join Election</MyAppText>
         <View style={styles.inputsContainer}>
-            <Input placeholder="Election Code" style={styles.input} />
-            <Input placeholder="ID" style={styles.input} />
-            <Input placeholder="Key" style={styles.input} />
-            <CustomizedButton title="Join" />
+            <Input placeholder="Election Code" style={styles.input} onChangeText={newText => setCode(newText)} />
+            <Input placeholder="ID" style={styles.input} onChangeText={newText => setId(newText)} />
+            <Input placeholder="Key" style={styles.input} onChangeText={newText => setKey(newText)} />
+            <CustomizedButton title="Join" onPress={() => console.log(code, id, key)} />
         </View>
     </LinearGradient>
   )
