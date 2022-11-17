dd641f4a6f0c186adda36d3dbcac3ec47a76e0f4
JaafarHawli <jaafarhawle@gmail.com>
2022-11-17 23:10:35 +0200

    app: linked app register api to frontend

---
+++ voteApp/api/register.js
@@ -0,0 +1,14 @@
+import axios from "./axios/axios";
+
+export const register = async (username, password) => {
+    const form = {
+        username: username,
+        password: password,
+    } 
+    try {
+        const data = await axios.post('app/register', form);
+        return data.data;
+      } catch (error) {
+        console.log(error);
+      }
+}
