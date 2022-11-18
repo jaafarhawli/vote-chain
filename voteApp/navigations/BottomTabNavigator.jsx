1833e5339113064d1e56814924ea6bfc75c098fa
JaafarHawli <jaafarhawle@gmail.com>
2022-11-18 10:14:59 +0200

    app: added icons to bottom tab navigator

--- voteApp/navigations/BottomTabNavigator.jsx
+++ voteApp/navigations/BottomTabNavigator.jsx
@@ -2,15 +2,34 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/Home/HomeScreen/HomeScreen';
 import ResultsScreen from '../screens/Home/ResultsScreen/ResultsScreen';
 import VoteScreen from '../screens/Home/VoteScreen/VoteScreen';
+import { Entypo, MaterialCommunityIcons, Foundation  } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
 function BottomTabNavigator() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Vote" component={VoteScreen} />
-      <Tab.Screen name="Results" component={ResultsScreen} />
+      <Tab.Screen name="Home" component={HomeScreen} 
+        options={{
+            tabBarIcon: () => (
+                <Entypo name="home" size={24} color="black" />
+            ),
+        }}
+      />
+      <Tab.Screen name="Vote" component={VoteScreen}
+        options={{
+            tabBarIcon: () => (
+                <MaterialCommunityIcons name="vote" size={24} color="black" />
+            ),
+        }}
+      />
+      <Tab.Screen name="Results" component={ResultsScreen}
+        options={{
+            tabBarIcon: () => (
+                <Foundation name="graph-bar" size={24} color="black" />
+            ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
