4a631c90d44cfbf55ca2800699587a6d18f8a231
JaafarHawli <jaafarhawle@gmail.com>
2022-11-18 09:55:32 +0200

    app: added check token useEffect

--- voteApp/App.js
+++ voteApp/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthNavigator from './navigations/AuthNavigator';
 import {useFonts} from 'expo-font'
+import BottomTabNavigator from './navigations/BottomTabNavigator';
+import * as SecureStore from 'expo-secure-store';
 
 export default function App() {
 
+  const [token, setToken] = useState();
+
+  const checkToken = async () => {
+    let result = await SecureStore.getItemAsync('token')
+    setToken(result)
+  }
+
+  useEffect(() => {
+    checkToken();
+  }, []);
+  
   const [fonts] = useFonts({
     AppFontBold: require('./assets/fonts/Quicksand-Bold.ttf'),
     AppFont: require('./assets/fonts/Quicksand-Regular.ttf'),
@@ -14,10 +27,14 @@ export default function App() {
 
   if(!fonts)
   return null;
-
+  
   return (
     <NavigationContainer>
+      {token ?
+      <BottomTabNavigator />
+      :
       <AuthNavigator />
+      }
     </NavigationContainer>
   );
 }
--- voteApp/navigations/BottomTabNavigator.jsx
+++ voteApp/navigations/BottomTabNavigator.jsx
@@ -1,4 +1,5 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from '../screens/Home/HomeScreen/HomeScreen';
 
 const Tab = createBottomTabNavigator();
 
