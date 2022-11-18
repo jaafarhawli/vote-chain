2f05327729b61040846041a31df5509cc33dcba7
JaafarHawli <jaafarhawle@gmail.com>
2022-11-18 14:08:44 +0200

    app: customized bottom tab navigator focus color

--- voteApp/navigations/BottomTabNavigator.jsx
+++ voteApp/navigations/BottomTabNavigator.jsx
@@ -4,6 +4,7 @@ import { HomeScreen, VoteScreen, ResultsScreen } from '../screens';
 import { Entypo, MaterialCommunityIcons, Foundation  } from '@expo/vector-icons';
 import { useSelector, useDispatch } from 'react-redux';
 import EmptyState from '../components/Complex/EmptyState/EmptyState';
+import { colors } from '../constants';
 
 const Tab = createBottomTabNavigator();
 
@@ -12,28 +13,25 @@ function BottomTabNavigator() {
   const election = useSelector((state) => state.election.value);
 
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Main" component={election.id == '' ? EmptyState : HomeScreen} empty={election.id == ''}
-        options={{
-            tabBarIcon: () => (
-                <Entypo name="home" size={24} color="black" />
-            ),
-        }}
-      />
-      <Tab.Screen name="Vote" component={election.id == '' ? EmptyState : VoteScreen}
-        options={{
-            tabBarIcon: () => (
-                <MaterialCommunityIcons name="vote" size={24} color="black" />
-            ),
-        }}
-      />
-      <Tab.Screen name="Results" component={election.id == '' ? EmptyState : ResultsScreen}
-        options={{
-            tabBarIcon: () => (
-                <Foundation name="graph-bar" size={24} color="black" />
-            ),
-        }}
-      />
+    <Tab.Navigator screenOptions={
+      ({route}) => ({
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: colors.secondary,
+        tabBarInactiveTintColor: colors.black[100],
+        tabBarIcon: ({color}) => {
+          let icon;
+          if (route.name === 'Main') {
+            return icon = <Entypo name="home" size={24} color={color} />
+          } else if (route.name === 'Vote') {
+            return icon = <MaterialCommunityIcons name="vote" size={24} color={color} />
+          } else if (route.name === 'Results') {
+            return icon = <Foundation name="graph-bar" size={24} color={color} />
+          }}
+      })
+    }>
+      <Tab.Screen name="Main" component={election.id == '' ? EmptyState : HomeScreen} empty={election.id == ''} />
+      <Tab.Screen name="Vote" component={election.id == '' ? EmptyState : VoteScreen} />
+      <Tab.Screen name="Results" component={election.id == '' ? EmptyState : ResultsScreen} />
     </Tab.Navigator>
   );
 }
