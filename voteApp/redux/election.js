8b0d174a22be1438779ad9cd53a4197e31a2cacc
JaafarHawli <jaafarhawle@gmail.com>
2022-11-18 11:34:08 +0200

    app: added redux election slice

---
+++ voteApp/redux/election.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const electionSlice = createSlice({
+    name: 'election',
+    initialState: {
+        value: {
+            id: '',
+            title: '',
+            startTime: '',
+            endTime: '',
+            code: '',
+            description: '',
+            launched: false,
+            address: ''
+        }
+    },
+    reducers: {
+        viewElection: (state, action) => {
+            state.value = {
+                ...state.value,
+                ...action.payload
+            }
+        }
+    }
+});
+
+export const {viewElection} = electionSlice.actions;
+
+export default electionSlice.reducer;
