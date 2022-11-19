07505b98e828ea56cee7f86e7d8b34f1f1bfd24c
JaafarHawli <jaafarhawle@gmail.com>
2022-11-19 15:49:11 +0200

    blockchain: migrated the new election contract

--- frontend/blockchain/test/Election.js
+++ frontend/blockchain/test/Election.js
@@ -5,13 +5,13 @@ contract('Election', (accounts) => {
     
     it('should deploy election', async() => {
         const admin_account = accounts[0];
-        const election = await Election.deployed(1668239079, 1668249079);
+        const election = await Election.deployed(1678239079, 1678249079);
         const admin = await election.admin.call();
         const start_time = await election.startTime.call();
         const end_time = await election.endTime.call();
         assert.equal(admin, admin_account);
-        assert.equal(start_time, 1668239079);
-        assert.equal(end_time, 1668249079);
+        assert.equal(start_time, 1678239079);
+        assert.equal(end_time, 1678249079);
     })
     
     it('should allow admin to add candidates', async() => {
@@ -45,6 +45,15 @@ contract('Election', (accounts) => {
         const voters = await election.voters.call(voter);
         assert(results[0].voteCount, 1);
         assert(voters.voted, true);
+    })
+
+    it('should be able to change election time interval', async() => {
+        const election = await Election.deployed(1668239079, 1668249079);
+        await election.changeTime(1678239079, 1678249079);
+        const start_time = await election.startTime.call();
+        const end_time = await election.endTime.call();
+        assert(start_time, 1678239079);
+        assert(end_time, 1678249079);
     })
     
 })
