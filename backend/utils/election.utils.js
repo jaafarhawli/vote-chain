1ec805372e1641ba101ae8935d591d454d084c7a
JaafarHawli <jaafarhawle@gmail.com>
2022-11-20 16:09:34 +0200

    backend: modified view elections as moderator function

--- backend/utils/election.utils.js
+++ backend/utils/election.utils.js
@@ -13,8 +13,8 @@ const viewElectionAsModeratorResult =  (election_id, user_id, res) => {
             title: election.title,
             start_time: election.start_time,
             end_time: election.end_time,
-            timezone: election.timezone,
-            voters: election.voters
+            voters: election.voters,
+            applying_voters: election.applying_voters
         }
         res.status(200).json({data: result});
     });
