5f09e9099a9bd51c2263f20c794be4ef3279a806
JaafarHawli <jaafarhawle@gmail.com>
2022-11-20 02:57:49 +0200

    frontend: added check if launched function to web3 client

--- frontend/src/Web3Client.js
+++ frontend/src/Web3Client.js
@@ -127,3 +127,14 @@ export const createElectionContract = async (start_time, end_time) => {
 	
 		return contract.methods.launchElection().send({from: selectedAccount});
 	}
+
+	export const checkIfLaunched = async (address) => {
+		if(!isInitialized)
+		await init();
+	
+		let provider = window.ethereum;
+		const web3 = new Web3(provider);
+		let contract = new web3.eth.Contract(ElectionContract.abi, address);
+
+		return contract.methods.checkIfLaunched().call();
+	}
