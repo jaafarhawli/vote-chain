041f39bb7d7442102847b90e0ce5445cb0b6c793
JaafarHawli <jaafarhawle@gmail.com>
2022-11-22 01:25:59 +0200

    frontend: modified add candidate modal

--- frontend/src/components/Modals/AddCandidate.jsx
+++ frontend/src/components/Modals/AddCandidate.jsx
@@ -84,7 +84,7 @@ const AddCandidate = ({open, closeModal}) => {
        
           <SuccessModal open={errorModal} message={error} error={isError} closeSuccess={() => setErrorModal(false)} />
        <div className=' fixed top-[50%] left-1/2 -translate-x-1/2 -translate-y-1/2 md:w-2/3 w-4/5 max-w-[500px] flex flex-col bg-white shadow-xl items-center z-10 rounded-lg px-8 py-14 '>
-           <IoClose className='fixed top-2 left-2 text-[30px] hover:bg-black-100/20 rounded-full duration-200 p-1' onClick={closeModal} />
+           <IoClose className='fixed top-2 left-2 text-[30px] hover:bg-black-100/20 rounded-full duration-200 p-1' onClick={() => {setErrorModal(false); closeModal()}} />
            <img src={logo} alt="logo" className='w-[180px]' />
         <div className='bg-black-100 h-[2px] w-[180px]'></div>  
         <h1 className='my-4 text-2xl font-semibold text-purple-100'>Add Candidate To Party</h1>  
