5ead4836323d6c2dc5760c35564693a86ffeb3f8
JaafarHawli <jaafarhawle@gmail.com>
2022-11-21 23:28:09 +0200

    frontend: added election redux to voter main page

--- frontend/src/pages/Vote/VoteMain.jsx
+++ frontend/src/pages/Vote/VoteMain.jsx
@@ -7,10 +7,12 @@ import curve from '../../assets/backgroundcurve.svg';
 import Button from '../../components/Reusable/Button';
 import LandingFooter from '../Landing/LandingFooter';
 import Timer from '../../components/Reusable/Timer';
+import { useSelector, useDispatch } from 'react-redux';
 
 const VoteMain = () => {
   
   const navigate = useNavigate();
+  const election = useSelector((state) => state.election.value);
   
   const [timerDays, setTimerDays] = useState();
   const [timerHours, setTimerHours] = useState();
@@ -21,7 +23,7 @@ const VoteMain = () => {
   const [ended, setEnded] = useState();
   
   const startEndTimer = () => {
-    const endDate = new Date(localStorage.election_end).getTime();
+    const endDate = new Date(election.endTime).getTime();
   
     let interval = setInterval(() => {
       const now = new Date().getTime();
@@ -47,7 +49,7 @@ const VoteMain = () => {
   }
 
   const startTimer = () => {
-    const startDate = new Date(localStorage.election_start).getTime();
+    const startDate = new Date(election.startTime).getTime();
 
     let interval = setInterval(() => {
       const now = new Date().getTime();
@@ -93,8 +95,8 @@ const VoteMain = () => {
       </div>
       <div className='flex h-full flex-1 lg:pl-28 md:pl-10 pl-4 sm:justify-between justify-center'>
         <div className='flex flex-col justify-center sm:items-start items-center text-center sm:text-left'>
-            <h1 className='text-4xl text-purple-100 font-bold md:text-5xl lg:text-6xl pt-12 md:pt-8'>{localStorage.election_title}</h1>
-            <p className='text-white pt-6 max-w-[400px]'>{localStorage.election_description}</p>
+            <h1 className='text-4xl text-purple-100 font-bold md:text-5xl lg:text-6xl pt-12 md:pt-8'>{election.title}</h1>
+            <p className='text-white pt-6 max-w-[400px]'>{election.description}</p>
             <div className='flex gap-4 mt-6'>
                 <Button onClick={() => navigate('select')} disabled={disabled} >Vote now</Button>              
                 <Button className={'border-cyan border-2 bg-opacity-0 hover:bg-cyan disabled:hover:bg-black-200 disabled:border-none'} disabled={disabled || !ended} onClick={checkResults} >Check Results</Button>
