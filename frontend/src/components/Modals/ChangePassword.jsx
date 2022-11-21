f96b0fffd99aa37cc1dc6a761fdb500e14039594
JaafarHawli <jaafarhawle@gmail.com>
2022-11-22 01:27:11 +0200

    frontend: modified change password modal

--- frontend/src/components/Modals/ChangePassword.jsx
+++ frontend/src/components/Modals/ChangePassword.jsx
@@ -65,7 +65,7 @@ const ChangePassword = ({open, closeModal}) => {
     <div className='bg-black-100/50 fixed w-full h-full z-10 '>
         <SuccessModal open={successModal} message={message} error={isError} closeSuccess={() => setSuccessModal(false)} />
      <div className=' fixed top-[50%] left-1/2 -translate-x-1/2 -translate-y-1/2 md:w-2/3 w-4/5 max-w-[500px] flex flex-col bg-white shadow-xl items-center z-10 rounded-lg px-8 py-14 '>
-         <IoClose className='fixed top-2 left-2 text-[30px] hover:bg-black-100/20 rounded-full duration-200 p-1' onClick={closeModal} />
+         <IoClose className='fixed top-2 left-2 text-[30px] hover:bg-black-100/20 rounded-full duration-200 p-1' onClick={() => {setSuccessModal(false); closeModal()}} />
          <img src={logo} alt="logo" className='w-[180px]' />
       <div className='bg-black-100 h-[2px] w-[180px]'></div>  
       <h1 className='my-4 text-2xl font-semibold text-purple-100'>Change Password</h1>  
