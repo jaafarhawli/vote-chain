f1c0f7c10bfe9fd07a4185fd145d4cdc702a7646
JaafarHawli <jaafarhawle@gmail.com>
2022-11-23 01:57:26 +0200

    frontend: added neon effect

--- frontend/src/pages/Landing/LandingSteps.jsx
+++ frontend/src/pages/Landing/LandingSteps.jsx
@@ -9,7 +9,7 @@ const LandingSteps = () => {
   return (
     <div className='flex flex-col bg-bg lg:px-28 md:px-10 px-4 items-center lg:flex-row lg:justify-between py-12'>
       <img src={banner} alt="/" className='lg:w-2/5 w-1/2 h-[250px] lg:h-[400px] object-cover rounded-lg opacity-90 min-w-[340px] lg:mt-12 mt-6' />
-      <div className='flex flex-col  text-center lg:w-1/2 border-[2px] rounded-xl border-purple-100 p-6'>
+      <div className='flex flex-col  text-center lg:w-1/2 border-[2px] rounded-xl border-purple-100 p-6 neon'>
         <h1 className='font-bold text-[32px] md:text-[36px] text-purple-100 lg:text-[40px]'>Build your own election</h1>
         <p className='pt-2 font-medium text-[20px] text-black-200'>Take control over the election, add some moderators to help you out</p>
         <div className='flex mt-8 gap-4 text-left h-fit'>
