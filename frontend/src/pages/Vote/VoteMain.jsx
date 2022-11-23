b75d389ebf142b020efb459dfb42a316bb4d7d9d
JaafarHawli <jaafarhawle@gmail.com>
2022-11-23 09:37:46 +0200

    frontend: changed voter main page hero style

--- frontend/src/pages/Vote/VoteMain.jsx
+++ frontend/src/pages/Vote/VoteMain.jsx
@@ -93,21 +93,19 @@ const VoteMain = () => {
 
   return (
     <>
-    <div className='hero flex flex-col bg-no-repeat bg-center bg-cover bg-purple-300 w-full' >
+    <div className='hero flex flex-col bg-no-repeat bg-center bg-cover bg-bg w-full h-screen' >
       <div className='flex w-full pt-4 items-center justify-between lg:px-28 md:px-10 px-4'>
         <img src={logo} alt="logo" className='w-48' />
       </div>
       <div className='flex h-full flex-1 lg:pl-28 md:pl-10 pl-4 sm:justify-between justify-center'>
         <div className='flex flex-col justify-center sm:items-start items-center text-center sm:text-left'>
-            <h1 className='text-4xl text-purple-100 font-bold md:text-5xl lg:text-6xl pt-12 md:pt-8'>{election.title}</h1>
+            <h1 className='text-4xl text-white font-bold md:text-5xl lg:text-6xl pt-12 md:pt-8'>{election.title}</h1>
             <p className='text-white pt-6 max-w-[400px]'>{election.description}</p>
             <div className='flex gap-4 mt-6'>
                 <Button onClick={() => navigate('select')} disabled={disabled || voter.voted} >Vote now</Button>              
                 <Button className={'border-cyan border-2 bg-opacity-0 hover:bg-cyan disabled:hover:bg-black-200 disabled:border-none'} disabled={disabled || !election.ended} onClick={checkResults} >Check Results</Button>
             </div> 
         </div>
-        <img src={landingsm} alt="talents" className="w-[45%] md:hidden hidden sm:flex mt-5" />
-        <img src={landinglg} alt="talents" className="w-[45%] hidden md:flex mt-5" />
       </div>
       <img src={curve} alt='/' className='w-full invisible max-h-[110px]' />
     </div>
