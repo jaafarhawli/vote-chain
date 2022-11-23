30b5eea48766fba233fedd02c0e3632bedefaac5
JaafarHawli <jaafarhawle@gmail.com>
2022-11-23 20:14:21 +0200

    frontend: added mui text field

--- frontend/src/components/Reusable/FormInput.jsx
+++ frontend/src/components/Reusable/FormInput.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import {TextField} from '@mui/material';
 
 const FormInput = (props) => {
+
   return (
-    <label>
-        <p className={`font-semibold mb-1 ${props.textStyle}`}>{props.children}</p>
-        <input className={props.className} type={props.type} onChange={props.onChange} defaultValue={props.defaultValue} />
-    </label>
+    <div className='w-full'>
+        {/* <p className={`font-semibold mb-1 ${props.textStyle}`}>{props.children}</p> */}
+        <TextField className={`w-full ${props.className}`} size='small' label={props.children} onChange={props.onChange} type={props.type} defaultValue={props.defaultValue} required={props.required} error={props.error} inputProps={{ style: { color: "white" } }} sx={{
+        "& .MuiInputLabel-root": {color: 'white'},
+        "& .MuiOutlinedInput-root": {
+        "& > fieldset": { borderColor: "white" },
+        '&:hover fieldset': {
+          borderColor: 'grey',
+        },
+        }}} />
+        {/* <input className={props.className} type={props.type} onChange={props.onChange} defaultValue={props.defaultValue} /> */}
+    </div>
   );
 }
 
