a1c035a0b26d1fd405a4deaab579f230362cae1c
JaafarHawli <jaafarhawle@gmail.com>
2022-11-23 17:59:42 +0200

    frontend: modified form input

--- frontend/src/components/Reusable/FormInput.jsx
+++ frontend/src/components/Reusable/FormInput.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const FormInput = (props) => {
   return (
     <label>
-        <p className={`font-semibold ${props.textStyle}`}>{props.children}</p>
+        <p className={`font-semibold mb-1 ${props.textStyle}`}>{props.children}</p>
         <input className={props.className} type={props.type} onChange={props.onChange} defaultValue={props.defaultValue} />
     </label>
   );
