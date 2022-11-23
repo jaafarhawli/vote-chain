5ca54de8e8d336e4d9267c757b059ec304f488a1
JaafarHawli <jaafarhawle@gmail.com>
2022-11-23 21:09:57 +0200

    frontend: modified form input component

--- frontend/src/components/Reusable/FormInput.jsx
+++ frontend/src/components/Reusable/FormInput.jsx
@@ -5,8 +5,7 @@ const FormInput = (props) => {
 
   return (
     <div className='w-full'>
-        {/* <p className={`font-semibold mb-1 ${props.textStyle}`}>{props.children}</p> */}
-        <TextField className={`w-full ${props.className}`} size='small' label={props.children} onChange={props.onChange} type={props.type} defaultValue={props.defaultValue} required={props.required} error={props.error} inputProps={{ style: { color: "white" } }} sx={{
+        <TextField className={`w-full ${props.className}`} size='small' label={props.children} onChange={props.onChange} type={props.type} defaultValue={props.defaultValue} required={props.required} disabled={props.disabled} error={props.error} inputProps={{ style: { color: "white" } }} sx={{
         "& .MuiInputLabel-root": {color: 'white'},
         "& .MuiOutlinedInput-root": {
         "& > fieldset": { borderColor: "white" },
@@ -14,7 +13,6 @@ const FormInput = (props) => {
           borderColor: 'grey',
         },
         }}} />
-        {/* <input className={props.className} type={props.type} onChange={props.onChange} defaultValue={props.defaultValue} /> */}
     </div>
   );
 }
