944c879d48fe036aa8eb75c41da6dcf7fb168b0b
JaafarHawli <jaafarhawle@gmail.com>
2022-11-23 15:45:19 +0200

    frontend: changed voter login page style

--- frontend/src/pages/Vote/VoteLoginForm.jsx
+++ frontend/src/pages/Vote/VoteLoginForm.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
-import logo from '../../assets/VOTE CHAIN-logo-black.png';
+import logo from '../../assets/VOTE CHAIN-logo-white.png';
 import Button from '../../components/Reusable/Button';
 import FormInput from '../../components/Reusable/FormInput';
 import axios from '../../api/axios';
@@ -77,15 +77,15 @@ const VoteLoginForm = () => {
   }
 
   return (
-    <div className='bg-gradient-to-br from-white/70 to-white/30 w-[450px]  rounded-xl flex flex-col items-center p-6 pb-10 backdrop-blur-2xl shadow-2xl before:absolute before:bg-white/[15%] before:inset-0 before:rotate-[-5deg] before:-z-[1] before:rounded-xl'>
+    <div className='bg-gradient-to-br from-white/30 to-white/10 w-[450px]  rounded-xl flex flex-col items-center p-6 pb-10 backdrop-blur-2xl shadow-2xl before:absolute before:bg-white/[15%] before:inset-0 before:rotate-[-5deg] before:-z-[1] before:rounded-xl neon'>
       <img src={logo} alt="logo" className='w-[180px]' />
-      <div className='bg-black-100 h-[2px] w-[180px]'></div>  
-      <h1 className='my-4 text-2xl font-semibold text-purple-100'>Welcome!</h1>  
+      <div className='bg-white h-[2px] w-[180px]'></div>  
+      <h1 className='my-4 text-2xl font-semibold text-white'>Welcome!</h1>  
       <h1 className={error? 'text-red ' : 'hidden'}>{message}</h1>
       <form className='w-4/5 flex flex-col gap-5 '>
-          <FormInput type="text" className='border-0' onChange={e => setCode(e.target.value)}>Election Code</FormInput>          
-          <FormInput type="text" className='border-0' onChange={e => setId(e.target.value)}>ID</FormInput>          
-          <FormInput type="text" className='border-0' onChange={e => setKey(e.target.value)}>Key</FormInput>                
+          <FormInput type="text" textStyle='text-purple-200' className='border-0' onChange={e => setCode(e.target.value)}>Election Code</FormInput>          
+          <FormInput type="text" textStyle='text-purple-200' className='border-0' onChange={e => setId(e.target.value)}>ID</FormInput>          
+          <FormInput type="text" textStyle='text-purple-200' className='border-0' onChange={e => setKey(e.target.value)}>Key</FormInput>                
           <Button className='bg-cyan' onClick={handleSubmit}>Join</Button>
       </form> 
     </div>
