7f8535040fa269c9c62d1f85f23181ed2c93bd2e
JaafarHawli <jaafarhawle@gmail.com>
2022-11-24 13:53:18 +0200

    frontend: added view passwrod icon to form input component

--- frontend/src/components/Reusable/FormInput.jsx
+++ frontend/src/components/Reusable/FormInput.jsx
@@ -1,20 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {TextField} from '@mui/material';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 const FormInput = (props) => {
 
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const handleClickShowPassword = () => {  
+      setShowPassword(!showPassword)
+  };
+
+
   return (
     <div className='w-full'>
-        <TextField className={`w-full ${props.className}`} size='small' label={props.children} onChange={props.onChange} type={props.type} defaultValue={props.defaultValue} required={props.required} disabled={props.disabled} error={props.error} inputProps={{ style: { color: "white" } }} sx={{
+        <TextField className={`w-full ${props.className}`} size='small' label={props.children} onChange={props.onChange} type={props.type==='password' ? showPassword ? 'text' : 'password' : props.type} defaultValue={props.defaultValue} required={props.required} disabled={props.disabled} error={props.error} inputProps={{ style: { color: "white" } }} sx={{
         "& .MuiInputLabel-root": {color: 'white'},
         "& .MuiOutlinedInput-root": {
         "& > fieldset": { borderColor: "white" },
         '&:hover fieldset': {
           borderColor: 'grey',
         },
-        }}} />
+        }}}
+        
+        InputProps=
+          {{endAdornment:(
+            props.type === 'password' ? 
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+                edge="end"
+              >
+              {showPassword ? <VisibilityOff className='text-white' /> : <Visibility className='text-white' />}
+              </IconButton>
+            </InputAdornment>
+            :
+            null
+          )}}  
+        />
     </div>
   );
 }
 
 export default FormInput;
+
+
+
