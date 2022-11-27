60387f64e299e78c12341a356e4ccd5f450711f0
JaafarHawli <jaafarhawle@gmail.com>
2022-11-27 02:42:31 +0200

    frontend: moved view time interval function to a separate file

--- frontend/src/Web3Client.js
+++ frontend/src/Web3Client.js
@@ -27,17 +27,6 @@ export const init = async () => {
 	  }
 } 
 
-	export const viewTimeInterval = async (address) => {
-		if(!isInitialized)
-		await init();
-	
-		let provider = window.ethereum;
-		const web3 = new Web3(provider);
-		let contract = new web3.eth.Contract(ElectionContract.abi, address);
-
-		return contract.methods.viewTimeInterval().call();
-	}
-
 	export const launchElection = async (address) => {
 		if(!isInitialized)
 		await init();
--- frontend/src/components/Reusable/BlockchainStatistics.jsx
+++ frontend/src/components/Reusable/BlockchainStatistics.jsx
@@ -3,7 +3,7 @@ import React, {useState, useEffect} from 'react';
 import { Doughnut } from 'react-chartjs-2';
 import { Bar } from 'react-chartjs-2';
 import { Pie } from 'react-chartjs-2';
-import { viewTimeInterval } from '../../Web3Client';
+import { viewTimeInterval } from '../../Web3/ViewTimeInterval';
 import { viewCandidates } from '../../Web3/ViewCandidates';
 import { viewVoters } from '../../Web3/ViewVoters';
 
