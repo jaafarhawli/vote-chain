5d2182dd352a54e6e6c9d5dc1591bf979aabc016
JaafarHawli <jaafarhawle@gmail.com>
2022-11-27 17:01:12 +0200

    frontend: added modal component to add moderator modal

--- frontend/src/components/Modals/AddModerator.jsx
+++ frontend/src/components/Modals/AddModerator.jsx
@@ -1,12 +1,11 @@
 /* eslint-disable */ 
 import React, {useState, useEffect} from 'react';
-import {IoClose} from 'react-icons/io5';
 import axios from '../../api/axios';
-import logo from '../../assets/VOTE CHAIN-logo-black.png';
 import Button from '../Reusable/Button';
 import SuccessModal from './SuccessModal';
 import { useSelector } from 'react-redux';
 import FormLabelInput from '../Reusable/FormLabelInput';
+import Modal from './Modal';
 
 const AddModerator = ({open, closeModal, refetch, socket}) => {
 
@@ -16,6 +15,7 @@ const AddModerator = ({open, closeModal, refetch, socket}) => {
     const [email, setEmail] = useState('');
     const [errorModal, setErrorModal] = useState(false);
     const [error, setError] = useState('');
+    const [isError, setIsError] = useState(true);
     const [disabled, setDisabled] = useState(true);
 
     const addModerator = async () => {
@@ -38,6 +38,7 @@ const AddModerator = ({open, closeModal, refetch, socket}) => {
               closeModal();
             } catch (error) {
                 setError(error);
+                setIsError(true);
                 setErrorModal(true);
               console.log(error);
             }  
@@ -54,19 +55,15 @@ const AddModerator = ({open, closeModal, refetch, socket}) => {
     return null;
 
   return (
-    <div className='bg-black-100/50 fixed w-full h-full z-10 '>
-        <SuccessModal open={errorModal} message={error} error={true} closeSuccess={() => setErrorModal(false)} />
-     <div className=' fixed top-[50%] left-1/2 -translate-x-1/2 -translate-y-1/2 md:w-2/3 w-4/5 max-w-[500px] flex flex-col bg-white shadow-xl items-center z-10 rounded-lg px-8 py-14 '>
-         <IoClose className='fixed top-2 left-2 text-[30px] hover:bg-black-100/20 rounded-full duration-200 p-1' onClick={closeModal} />
-         <img src={logo} alt="logo" className='w-[180px]' />
-      <div className='bg-black-100 h-[2px] w-[180px]'></div>  
-      <h1 className='my-4 text-2xl font-semibold text-purple-100'>Add Moderator</h1>  
-      <form className='w-4/5 flex flex-col gap-5 '>
-          <FormLabelInput type="text" onChange={e => setEmail(e.target.value)}>Moderator Email</FormLabelInput>
-          <Button className='bg-cyan' onClick={addModerator} disabled={disabled} >Add</Button>
-      </form> 
-     </div>
-    </div>
+    <div>
+        <Modal title={'Add Moderator'} closeModal={closeModal} content={
+          <form className='w-4/5 flex flex-col gap-5 '>
+            <FormLabelInput type="text" onChange={e => setEmail(e.target.value)}>Moderator Email</FormLabelInput>
+            <Button className='bg-cyan' onClick={addModerator} disabled={disabled} >Add</Button>
+          </form> 
+        } />
+        <SuccessModal open={errorModal} message={error} error={isError} closeSuccess={() => setErrorModal(false)} />
+    </div> 
   );
 }
 
