94c3f6d24c0cfcfdc9cdbc745fdb76c4bf5eda1b
JaafarHawli <jaafarhawle@gmail.com>
2022-11-27 16:55:48 +0200

    frontend: added modal file

--- frontend/src/components/Modals/AddCandidate.jsx
+++ frontend/src/components/Modals/AddCandidate.jsx
@@ -1,12 +1,11 @@
 import React, {useState, useRef} from 'react';
-import {IoClose} from 'react-icons/io5';
 import axios from '../../api/axios';
-import logo from '../../assets/VOTE CHAIN-logo-black.png';
 import Button from '../Reusable/Button';
 import SuccessModal from './SuccessModal';
 import {IoMdImage} from 'react-icons/io';
 import { useSelector } from 'react-redux';
 import FormLabelInput from '../Reusable/FormLabelInput';
+import Modal from './Modal';
 
 const AddCandidate = ({open, closeModal}) => {
 
@@ -80,21 +79,16 @@ const AddCandidate = ({open, closeModal}) => {
       return null;
 
     return (
-      <div className='bg-black-100/50 fixed w-full h-full z-10 '>
-       
-          <SuccessModal open={errorModal} message={error} error={isError} closeSuccess={() => setErrorModal(false)} />
-       <div className=' fixed top-[50%] left-1/2 -translate-x-1/2 -translate-y-1/2 md:w-2/3 w-4/5 max-w-[500px] flex flex-col bg-white shadow-xl items-center z-10 rounded-lg px-8 py-14 '>
-           <IoClose className='fixed top-2 left-2 text-[30px] hover:bg-black-100/20 rounded-full duration-200 p-1' onClick={() => {setErrorModal(false); closeModal()}} />
-           <img src={logo} alt="logo" className='w-[180px]' />
-        <div className='bg-black-100 h-[2px] w-[180px]'></div>  
-        <h1 className='my-4 text-2xl font-semibold text-purple-100'>Add Candidate To Party</h1>  
-        <form className='w-4/5 flex flex-col gap-5 '>
-            <input type="file" onChange={e => setImage(e.target.files[0])} className='hidden' ref={imageRef} />
-            <IoMdImage className='self-center text-[100px] bg-black-100/20 text-white p-4 rounded-full hover:bg-black-100/30 duration-150' onClick={handleClick} />
-            <FormLabelInput type="text" onChange={e => setName(e.target.value)}>Candidate Name</FormLabelInput>
-            <Button className='bg-cyan' onClick={addCandidate}>Add</Button>
-        </form> 
-       </div>
+      <div>
+        <Modal title={'Add Candidate To Party'} closeModal={closeModal} content={
+          <form className='w-4/5 flex flex-col gap-5 '>
+              <input type="file" onChange={e => setImage(e.target.files[0])} className='hidden' ref={imageRef} />
+              <IoMdImage className='self-center text-[100px] bg-black-100/20 text-white p-4 rounded-full hover:bg-black-100/30 duration-150' onClick={handleClick} />
+              <FormLabelInput type="text" onChange={e => setName(e.target.value)}>Candidate Name</FormLabelInput>
+              <Button className='bg-cyan' onClick={addCandidate}>Add</Button>
+          </form> 
+        } />
+        <SuccessModal open={errorModal} message={error} error={isError} closeSuccess={() => setErrorModal(false)} />
       </div>
     );
 }
