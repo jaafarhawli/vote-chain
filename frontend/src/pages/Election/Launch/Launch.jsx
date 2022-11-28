4f600b943cf35a15b5233da00f95ce6d3edabafa
JaafarHawli <jaafarhawle@gmail.com>
2022-11-28 17:36:48 +0200

    frontend: added launch election function to a separate file

--- frontend/src/pages/Election/Launch/Launch.jsx
+++ frontend/src/pages/Election/Launch/Launch.jsx
@@ -1,87 +1,27 @@
 import React, {useState} from 'react';
 import Button from '../../../components/Reusable/Button';
-import ConfirmModal from '../../../components/Modals/ConfirmModal';
-import axios from '../../../api/axios';
+import {ConfirmModal} from '../../../components/Modals';
 import {useQuery} from '@tanstack/react-query';
-import { addCandidates, addVotersToBlockchain as addVoters, launchElection as launchToBlockchain } from '../../../Web3';
 import { useSelector, useDispatch } from 'react-redux';
-import { viewElection } from '../../../redux/election';
+import { viewVoters } from '../../../api/viewVoters';
+import { viewCandidates } from '../../../api/viewCandidates';
+import { launchElection } from './LaunchElection';
 
 const Launch = () => {
 
-    const election = useSelector((state) => state.election.value);
-    const user = useSelector((state) => state.user.value);
-    const dispatch = useDispatch();
-
-    const [confirmModal, setConfirmModal] = useState(false);
-    const [disabled, setDisabled] = useState(election.launched===true);
-
-    const {data} = useQuery(["candidates"], async () => {
-      return axios.get(`candidate/${election.id}`, {
-                  headers: {
-                    Authorization: `bearer ${localStorage.token}`
-                  }
-                }).then((res) => res.data.data);
-  })
-
-  const {data: voters} = useQuery(["voters"], async () => {
-    return axios.get(`voter/voters/${election.id}`, {
-                headers: {
-                  Authorization: `bearer ${localStorage.token}`
-                }
-              }).then((res) => res.data.data);
-})
-
-  const addCandidatesToBlockchain = async () => {
-    let candidates = [];
-    let parties = [];
-    for(let candidate of data) {
-      candidates.push(candidate.name);
-      parties.push(candidate.party);
-    }
-    await addCandidates(candidates, parties, election.address);
-  }
+  const election = useSelector((state) => state.election.value);
+  const user = useSelector((state) => state.user.value);
+  const dispatch = useDispatch();
+  const [confirmModal, setConfirmModal] = useState(false);
+  const [disabled, setDisabled] = useState(election.launched===true);
   
-  const addVotersToBlockChain = async () => {
-    let voters_addresses = [];
-    for(let voter of voters) {
-      voters_addresses.push(voter.voter_wallet_address);
-    }
-    await addVoters(voters_addresses, election.address);
-  }
+  const {data} = useQuery(["candidates"], () => viewCandidates(election.id));
 
-    
-    const closeConfirm = () => {
-        setConfirmModal(false);
-    }
-
-    const launchElection = async () => {
-        const form = {
-            election_id: election.id,
-            user_id: user.id 
-        }
-        await addCandidatesToBlockchain();
-        await addVotersToBlockChain();
-        await launchToBlockchain(election.address);     
-        try {
-            await axios.put('election/launch', form, {
-                headers: {
-                  Authorization: `bearer ${localStorage.token}`
-                }
-              });
-              dispatch(viewElection({
-                launched: true,
-              }));
-              setDisabled(true);
-              closeConfirm();
-            } catch (error) {
-              console.log(error);
-            }
-        }
+  const {data: voters} = useQuery(["voters"], () => viewVoters(election.id));
 
   return (
     <>
-    <ConfirmModal  open={confirmModal} closeModal={closeConfirm} click={launchElection} launch={true} text={"Are you sure you want to launch this election?"} />
+    <ConfirmModal  open={confirmModal} closeModal={() => setConfirmModal(false)} click={() => launchElection(election.id, user.id, election.address, data, voters, dispatch, setDisabled, setConfirmModal)} launch={true} text={"Are you sure you want to launch this election?"} />
     <div className='pl-[250px] pt-[150px] w-full bg-purple-400 min-h-screen'>
     <div className='w-[98%] mx-auto px-8'>
         <h1 className='text-[28px] font-bold'>Launch Election</h1>
